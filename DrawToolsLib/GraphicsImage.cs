907377f94017c549e32f60f6cefcbcd93cfe2078
Caelan Sayler <caelan@tireacquire.com>
2015-10-14 23:21:12 +0000

    Fixed bug where keybindings would handle keydown events in textbox's preventing input. Add GraphicsImage class
    git-tfs-id: [https://caesay.visualstudio.com/DefaultCollection]$/Clowd;C45
    
    Former-commit-id: 9efa532cbeddd15b5256e80a36fb492609625bdc

---
+++ DrawToolsLib/GraphicsImage.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DrawToolsLib
+{
+    public class GraphicsImage : GraphicsRectangleBase
+    {
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+
+        private string fileName;
+        private BitmapSource imageCache;
+        public GraphicsImage(string filename, double left, double top, double right, double bottom,
+            double lineWidth, Color objectColor, double actualScale)
+        {
+            this.rectangleLeft = left;
+            this.rectangleTop = top;
+            this.rectangleRight = right;
+            this.rectangleBottom = bottom;
+            this.graphicsLineWidth = lineWidth;
+            this.graphicsObjectColor = objectColor;
+            this.graphicsActualScale = actualScale;
+            this.fileName = filename;
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException(filename);
+
+            BitmapSource myImage = BitmapFrame.Create(
+                new Uri(filename, UriKind.Absolute),
+                BitmapCreateOptions.None,
+                BitmapCacheOption.OnLoad);
+
+            imageCache = myImage;
+
+            //RefreshDrawng();
+        }
+
+        public override void Draw(DrawingContext drawingContext)
+        {
+            if (drawingContext == null)
+            {
+                throw new ArgumentNullException("drawingContext");
+            }
+
+            Rect r = Rectangle;
+            drawingContext.DrawRectangle(Brushes.Pink, new Pen(), r);
+            drawingContext.DrawImage(imageCache, r);
+
+            base.Draw(drawingContext);
+        }
+
+        public override bool Contains(Point point)
+        {
+            return this.Rectangle.Contains(point);
+        }
+
+        public override PropertiesGraphicsBase CreateSerializedObject()
+        {
+            return new PropertiesGraphicsImage(this);
+        }
+    }
+}
