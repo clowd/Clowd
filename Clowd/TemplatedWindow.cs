588cf3c4385bb0bb4541be4e83389669223cc08b
Caelan <git@caesay.com>
2015-10-14 14:31:22 +0000

    add files i forgot to check in last time
    git-tfs-id: [https://caesay.visualstudio.com/DefaultCollection]$/Clowd;C43
    
    Former-commit-id: 9309a0bcae231f85eaf1f319adcc727d32f02943

---
+++ Clowd/TemplatedWindow.cs
@@ -0,0 +1,92 @@
+﻿using Clowd.Utilities;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Clowd
+{
+    public class TemplatedWindow
+    {
+        public static Window CreateWindow(bool customChrome, string title, Control content)
+        {
+            double titleHeight, borderWidth;
+            Window window;
+            if (customChrome)
+            {
+                window = CreateMetroWindow();
+                titleHeight = DpiScale.UpScaleY(40);
+                borderWidth = 1;
+            }
+            else
+            {
+                window = CreateNormalWindow();
+                titleHeight = SystemParameters.WindowCaptionHeight + SystemParameters.ResizeFrameHorizontalBorderHeight;
+                borderWidth = SystemParameters.ResizeFrameVerticalBorderWidth;
+            }
+            var tcc = new Controls.TransitioningContentControl();
+            tcc.HorizontalAlignment = HorizontalAlignment.Stretch;
+            tcc.VerticalAlignment = VerticalAlignment.Stretch;
+
+            window.Content = tcc;
+            tcc.Content = content;
+
+            window.Title = title;
+            window.Height = content.Height + titleHeight + borderWidth;
+            window.Width = content.Width + borderWidth * 2;
+            content.Width = Double.NaN;
+            content.Height = Double.NaN;
+            content.VerticalAlignment = VerticalAlignment.Stretch;
+            content.HorizontalAlignment = HorizontalAlignment.Stretch;
+
+            return window;
+        }
+        public static void SetContent(Window window, Control content)
+        {
+            var tcc = (Controls.TransitioningContentControl)window.Content;
+            tcc.Content = content;
+        }
+        public static void SetContentToSpinner(Window window)
+        {
+            var tcc = (Controls.TransitioningContentControl)window.Content;
+            Style style = Application.Current.FindResource("CircleProgressRingStyle") as Style;
+            Controls.ProgressRing pr = new Controls.ProgressRing();
+            pr.IsActive = true;
+            pr.Style = style;
+            pr.Width = 100;
+            pr.SetResourceReference(Controls.ProgressRing.ForegroundProperty, "AccentColorBrush");
+            pr.Height = 100;
+            pr.HorizontalAlignment = HorizontalAlignment.Center;
+            pr.VerticalAlignment = VerticalAlignment.Center;
+            tcc.Content = pr;
+        }
+        public static Window GetWindow(Control content)
+        {
+            return Window.GetWindow(content);
+        }
+
+        private static MetroWindow CreateMetroWindow()
+        {
+            MetroWindow mw = new MetroWindow();
+            mw.IconTemplate = (DataTemplate)App.Singleton.FindResource("MetroIconTemplate");
+            mw.ResizeMode = ResizeMode.CanResizeWithGrip;
+            mw.BorderThickness = new Thickness(1, 1, 1, 1);
+            mw.SetResourceReference(MetroWindow.BorderBrushProperty, "WindowTitleColorBrush");
+            mw.WindowTransitionsEnabled = false;
+            mw.Icon = new BitmapImage(new Uri("pack://application:,,,/Images/default.ico"));
+            return mw;
+        }
+        private static Window CreateNormalWindow()
+        {
+            Window w = new Window();
+            w.Icon = new BitmapImage(new Uri("pack://application:,,,/Images/default.ico"));
+            return w;
+        }
+    }
+}
---
+++ Clowd/Utilities/RegistryOptions.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowd.Utilities
+{
+    public static class RegistryOptions
+    {
+        private const string ContextMenuShellName = "Upload with Clowd";
+        private const string AppName = "Clowd";
+        private static readonly string[] ContextMenuInstallLocations = new[]
+        {
+            @"*\shell",
+            @"Directory\shell"
+        };
+
+        public static bool GetAutoStart()
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            return registryKey.GetValue(AppName) != null;
+        }
+        public static void SetAutoStart(bool startWithWindows)
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (startWithWindows)
+            {
+                registryKey.SetValue(AppName, Assembly.GetExecutingAssembly().Location);
+            }
+            else
+            {
+                registryKey.DeleteValue(AppName);
+            }
+        }
+        public static bool GetContextMenu()
+        {
+            using (var view32 = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry32))
+            using (var clsid32 = view32.OpenSubKey(@"Software\Classes\", true))
+            {
+                foreach (var location in ContextMenuInstallLocations)
+                {
+                    using (var subkey = clsid32.CreateSubKey(location))
+                    {
+                        if (CheckInstalledContextMenu(subkey))
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static void SetContextMenu(bool enabled)
+        {
+            using (var view32 = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry32))
+            using (var clsid32 = view32.OpenSubKey(@"Software\Classes\", true))
+            {
+                foreach (var location in ContextMenuInstallLocations)
+                {
+                    using (var subkey = clsid32.CreateSubKey(location))
+                    {
+                        if (enabled)
+                            InstallContextMenu(subkey);
+                        else
+                            UninstallContextMenu(subkey);
+                    }
+                }
+            }
+        }
+
+        private static void InstallContextMenu(RegistryKey shellRoot)
+        {
+            var file = Assembly.GetExecutingAssembly().Location;
+            using (var clowd = shellRoot.CreateSubKey(ContextMenuShellName))
+            {
+                clowd.SetValue("Icon", file, RegistryValueKind.String);
+                using (var command = clowd.CreateSubKey("command"))
+                {
+                    command.SetValue("", file);
+                }
+            }
+        }
+        private static bool CheckInstalledContextMenu(RegistryKey shellRoot)
+        {
+            return shellRoot.OpenSubKey(ContextMenuShellName) != null;
+        }
+        private static void UninstallContextMenu(RegistryKey shellRoot)
+        {
+            shellRoot.DeleteSubKeyTree(ContextMenuShellName, false);
+        }
+    }
+}
