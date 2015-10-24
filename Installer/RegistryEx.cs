3c5722f0badd5acb49be5ab71a32c8e42c637415
Caelan Sayler <caelan@tireacquire.com>
2015-10-24 20:07:00 +0000

    Import Screeney library, Initial check-in on installer work.
    git-tfs-id: [https://caesay.visualstudio.com/DefaultCollection]$/Clowd;C50

---
+++ Installer/RegistryEx.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace Clowd.Installer
+{
+    public class RegistryEx
+    {
+        public static string GetInstallPath(InstallMode mode)
+        {
+            if (mode == InstallMode.System)
+                return Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            if (mode == InstallMode.System)
+                return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            throw new InvalidOperationException();
+        }
+        public static RegistryKey[] GetClassesRoot(RegistryQuery location)
+        {
+            var path = @"Software\Classes\";
+            return OpenKeysFromRootPath(path, location);
+        }
+        public static RegistryKey[] OpenKeysFromRootPath(string path, RegistryQuery location, bool writable = true)
+        {
+            List<RegistryKey> keys = new List<RegistryKey>();
+            if (location == RegistryQuery.AllUsers || location == RegistryQuery.AllUsersAndSystem)
+            {
+                foreach (var user in GetAllSystemUsers())
+                {
+                    var sid = GetSIDFromUserName(user);
+                    var k = Registry.Users.OpenSubKey(sid + "\\" + path, writable);
+                    if (k != null)
+                        keys.Add(k);
+                }
+            }
+            else if (location == RegistryQuery.CurrentUser)
+            {
+                var k = Registry.CurrentUser.OpenSubKey(path, writable);
+                if (k != null)
+                    keys.Add(k);
+            }
+            if (location == RegistryQuery.System || location == RegistryQuery.AllUsersAndSystem)
+            {
+                var k = Registry.LocalMachine.OpenSubKey(path, writable);
+                if (k != null)
+                    keys.Add(k);
+            }
+            return keys.ToArray();
+        }
+        public static RegistryKey CreateKeyFromRootPath(string path, InstallMode location)
+        {
+            if (location == InstallMode.CurrentUser)
+            {
+                var k = Registry.CurrentUser.OpenSubKey(path, true) ?? Registry.CurrentUser.CreateSubKey(path);
+                return k;
+            }
+            else if (location == InstallMode.System)
+            {
+                var k = Registry.LocalMachine.OpenSubKey(path, true) ?? Registry.LocalMachine.CreateSubKey(path);
+                return k;
+            }
+            throw new InvalidOperationException();
+        }
+        private static string GetSIDFromUserName(string userName)
+        {
+            var account = new System.Security.Principal.NTAccount(userName);
+            var identifier = (System.Security.Principal.SecurityIdentifier)account.Translate(typeof(System.Security.Principal.SecurityIdentifier));
+            var sid = identifier.Value;
+            return sid;
+        }
+        private static string[] GetAllSystemUsers()
+        {
+            List<string> names = new List<string>();
+            SelectQuery query = new SelectQuery("Win32_UserAccount");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            foreach (ManagementObject envVar in searcher.Get())
+            {
+                names.Add((string)envVar["Name"]);
+            }
+            return names.ToArray();
+        }
+    }
+    public enum RegistryQuery
+    {
+        System = 0,
+        CurrentUser = 1,
+        AllUsers,
+        AllUsersAndSystem
+    }
+    public enum InstallMode
+    {
+        System = 0,
+        CurrentUser = 1,
+    }
+    public enum InstallQuery
+    {
+        System = 0,
+        CurrentUser = 1,
+        CurrentAndOtherUsers,
+        OtherUsers,
+        None,
+    }
+}
--- QuickShareServer/QuickShareModel.cs
+++ QuickShareServer/QuickShareModel.cs
@@ -9,21 +9,15 @@ namespace QuickShareServer
   [Serializable]
   [System.CodeDom.Compiler.GeneratedCode("LightSpeedModelGenerator", "1.0.0.0")]
   [System.ComponentModel.DataObject]
-  [Table("Uploads")]
   public partial class Upload : Entity<long>
   {
+    // Some entity properties were not generated because this model exceeds the
+    // number of entity types supported in this edition of LightSpeed.  See
+    // http://www.mindscape.co.nz/products/lightspeed/licensing.aspx for more information.
+    
     #region Fields
   
     #pragma warning disable 169
-    private System.DateTime _uploadDate;
-    private System.Nullable<System.DateTime> _lastAccessed;
-    private System.Nullable<int> _maxViews;
-    private int _views;
-    private bool _hidden;
-    private string _displayName;
-    private string _mimeType;
-    private string _storageKey;
-    private System.Nullable<System.DateTime> _validUntil;
     private System.Nullable<long> _ownerId;
     #pragma warning restore 169
 
@@ -31,24 +25,6 @@ namespace QuickShareServer
     
     #region Field attribute and view names
     
-    /// <summary>Identifies the UploadDate entity attribute.</summary>
-    public const string UploadDateField = "UploadDate";
-    /// <summary>Identifies the LastAccessed entity attribute.</summary>
-    public const string LastAccessedField = "LastAccessed";
-    /// <summary>Identifies the MaxViews entity attribute.</summary>
-    public const string MaxViewsField = "MaxViews";
-    /// <summary>Identifies the Views entity attribute.</summary>
-    public const string ViewsField = "Views";
-    /// <summary>Identifies the Hidden entity attribute.</summary>
-    public const string HiddenField = "Hidden";
-    /// <summary>Identifies the DisplayName entity attribute.</summary>
-    public const string DisplayNameField = "DisplayName";
-    /// <summary>Identifies the MimeType entity attribute.</summary>
-    public const string MimeTypeField = "MimeType";
-    /// <summary>Identifies the StorageKey entity attribute.</summary>
-    public const string StorageKeyField = "StorageKey";
-    /// <summary>Identifies the ValidUntil entity attribute.</summary>
-    public const string ValidUntilField = "ValidUntil";
     /// <summary>Identifies the OwnerId entity attribute.</summary>
     public const string OwnerIdField = "OwnerId";
 
@@ -73,69 +49,6 @@ namespace QuickShareServer
     }
 
 
-    [System.Diagnostics.DebuggerNonUserCode]
-    public System.DateTime UploadDate
-    {
-      get { return Get(ref _uploadDate, "UploadDate"); }
-      set { Set(ref _uploadDate, value, "UploadDate"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public System.Nullable<System.DateTime> LastAccessed
-    {
-      get { return Get(ref _lastAccessed, "LastAccessed"); }
-      set { Set(ref _lastAccessed, value, "LastAccessed"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public System.Nullable<int> MaxViews
-    {
-      get { return Get(ref _maxViews, "MaxViews"); }
-      set { Set(ref _maxViews, value, "MaxViews"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public int Views
-    {
-      get { return Get(ref _views, "Views"); }
-      set { Set(ref _views, value, "Views"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public bool Hidden
-    {
-      get { return Get(ref _hidden, "Hidden"); }
-      set { Set(ref _hidden, value, "Hidden"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public string DisplayName
-    {
-      get { return Get(ref _displayName, "DisplayName"); }
-      set { Set(ref _displayName, value, "DisplayName"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public string MimeType
-    {
-      get { return Get(ref _mimeType, "MimeType"); }
-      set { Set(ref _mimeType, value, "MimeType"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public string StorageKey
-    {
-      get { return Get(ref _storageKey, "StorageKey"); }
-      set { Set(ref _storageKey, value, "StorageKey"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public System.Nullable<System.DateTime> ValidUntil
-    {
-      get { return Get(ref _validUntil, "ValidUntil"); }
-      set { Set(ref _validUntil, value, "ValidUntil"); }
-    }
-
     /// <summary>Gets or sets the ID for the <see cref="Owner" /> property.</summary>
     [System.Diagnostics.DebuggerNonUserCode]
     public System.Nullable<long> OwnerId
@@ -151,43 +64,11 @@ namespace QuickShareServer
   [Serializable]
   [System.CodeDom.Compiler.GeneratedCode("LightSpeedModelGenerator", "1.0.0.0")]
   [System.ComponentModel.DataObject]
-  [Table("Users")]
   public partial class User : Entity<long>
   {
-    #region Fields
-  
-    #pragma warning disable 169
-    private string _username;
-    private string _password;
-    private string _salt;
-    [ValueField]
-    private ModelUserDefinedTypes.SubscriptionType _subscription;
-    private System.DateTime _subscriptionPeriod;
-    private System.DateTime _createdDate;
-    private string _email;
-    #pragma warning restore 169
-
-    #endregion
-    
-    #region Field attribute and view names
-    
-    /// <summary>Identifies the Username entity attribute.</summary>
-    public const string UsernameField = "Username";
-    /// <summary>Identifies the Password entity attribute.</summary>
-    public const string PasswordField = "Password";
-    /// <summary>Identifies the Salt entity attribute.</summary>
-    public const string SaltField = "Salt";
-    /// <summary>Identifies the Subscription entity attribute.</summary>
-    public const string SubscriptionField = "Subscription";
-    /// <summary>Identifies the SubscriptionPeriod entity attribute.</summary>
-    public const string SubscriptionPeriodField = "SubscriptionPeriod";
-    /// <summary>Identifies the CreatedDate entity attribute.</summary>
-    public const string CreatedDateField = "CreatedDate";
-    /// <summary>Identifies the Email entity attribute.</summary>
-    public const string EmailField = "Email";
-
-
-    #endregion
+    // Some entity properties were not generated because this model exceeds the
+    // number of entity types supported in this edition of LightSpeed.  See
+    // http://www.mindscape.co.nz/products/lightspeed/licensing.aspx for more information.
     
     #region Relationships
 
@@ -206,58 +87,12 @@ namespace QuickShareServer
     }
 
 
-    [System.Diagnostics.DebuggerNonUserCode]
-    public string Username
-    {
-      get { return Get(ref _username, "Username"); }
-      set { Set(ref _username, value, "Username"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public string Password
-    {
-      get { return Get(ref _password, "Password"); }
-      set { Set(ref _password, value, "Password"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public string Salt
-    {
-      get { return Get(ref _salt, "Salt"); }
-      set { Set(ref _salt, value, "Salt"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public ModelUserDefinedTypes.SubscriptionType Subscription
-    {
-      get { return Get(ref _subscription, "Subscription"); }
-      set { Set(ref _subscription, value, "Subscription"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public System.DateTime SubscriptionPeriod
-    {
-      get { return Get(ref _subscriptionPeriod, "SubscriptionPeriod"); }
-      set { Set(ref _subscriptionPeriod, value, "SubscriptionPeriod"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public System.DateTime CreatedDate
-    {
-      get { return Get(ref _createdDate, "CreatedDate"); }
-      set { Set(ref _createdDate, value, "CreatedDate"); }
-    }
-
-    [System.Diagnostics.DebuggerNonUserCode]
-    public string Email
-    {
-      get { return Get(ref _email, "Email"); }
-      set { Set(ref _email, value, "Email"); }
-    }
 
     #endregion
   }
 
+
+#error Number of entity types exceeds that supported in this edition of LightSpeed.  See http://www.mindscape.co.nz/products/lightspeed/licensing.aspx.
 
 
 
