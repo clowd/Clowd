04346b8bd273218b0593b568bd20af6b7f5c2dc4
Caelan Sayler <caelan@tireacquire.com>
2015-10-26 19:25:11 +0000

    added a bunch of stuff, incl native screeney dependancies
    git-tfs-id: [https://caesay.visualstudio.com/DefaultCollection]$/Clowd;C52

---
+++ Installer/Pages/LocationsPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clowd.Installer
+{
+    /// <summary>
+    /// Interaction logic for LocationsPage.xaml
+    /// </summary>
+    public partial class LocationsPage : Page
+    {
+        public LocationsPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Local_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav = NavigationService.GetNavigationService(this);
+            nav?.Navigate(new FeaturesPage());
+        }
+
+        private void Global_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav = NavigationService.GetNavigationService(this);
+            nav?.Navigate(new FeaturesPage());
+        }
+    }
+}
