abeee2eaf2e292d6d39e3c1d69a36c3c4524a21f
Caelan Sayler <caelan@tireacquire.com>
2015-11-08 01:25:55 +0000

    Initial work on the new TaskWindow, misc bug fixes.
    git-tfs-id: [https://caesay.visualstudio.com/DefaultCollection]$/Clowd;C62

---
+++ Clowd/Controls/CornerClippingBorder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Clowd.Controls
+{
+    /// <Remarks>
+    ///     As a side effect ClippingBorder will surpress any databinding or animation of 
+    ///         its childs UIElement.Clip property until the child is removed from ClippingBorder
+    /// </Remarks>
+    public class CornerClippingBorder : Border
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            OnApplyChildClip();
+            base.OnRender(dc);
+        }
+
+        public override UIElement Child
+        {
+            get
+            {
+                return base.Child;
+            }
+            set
+            {
+                if (this.Child != value)
+                {
+                    if (this.Child != null)
+                    {
+                        // Restore original clipping
+                        this.Child.SetValue(UIElement.ClipProperty, _oldClip);
+                    }
+
+                    if (value != null)
+                    {
+                        _oldClip = value.ReadLocalValue(UIElement.ClipProperty);
+                    }
+                    else
+                    {
+                        // If we dont set it to null we could leak a Geometry object
+                        _oldClip = null;
+                    }
+
+                    base.Child = value;
+                }
+            }
+        }
+
+        protected virtual void OnApplyChildClip()
+        {
+            UIElement child = this.Child;
+            if (child != null)
+            {
+                _clipRect.RadiusX = _clipRect.RadiusY = Math.Max(0.0, this.CornerRadius.TopLeft - (this.BorderThickness.Left * 0.5));
+                _clipRect.Rect = new Rect(Child.RenderSize);
+                child.Clip = _clipRect;
+            }
+        }
+
+        private RectangleGeometry _clipRect = new RectangleGeometry();
+        private object _oldClip;
+    }
+}
---
+++ Clowd/Utilities/TrulyObservableCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace Clowd.Utilities
+{
+    public sealed class TrulyObservableCollection<T> : ObservableCollection<T>, INotifyPropertyChanged
+        where T : INotifyPropertyChanged
+    {
+        public new event PropertyChangedEventHandler PropertyChanged
+        {
+            add { base.PropertyChanged += value; }
+            remove { base.PropertyChanged -= value; }
+        }
+
+        public TrulyObservableCollection()
+            : base()
+        {
+            this.CollectionChanged += this.TrulyObservableCollectionCollectionChanged;
+        }
+
+        public void TrulyObservableCollectionCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //this is kind of stupid, because it will only ever be one item but because NetItems is an array we need to 
+            //enumerate it in case this is ever implemented.
+            if (e.NewItems != null)
+            {
+                foreach (Object item in e.NewItems)
+                {
+                    (item as INotifyPropertyChanged).PropertyChanged += this.ItemPropertyChanged;
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (Object item in e.OldItems)
+                {
+                    (item as INotifyPropertyChanged).PropertyChanged -= this.ItemPropertyChanged;
+                }
+            }
+        }
+
+        public void AddRange(IEnumerable<T> dataToAdd)
+        {
+            this.CheckReentrancy();
+
+            //int startingIndex = this.Count;
+
+            foreach (var data in dataToAdd)
+            {
+                this.Items.Add(data);
+            }
+
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        private void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
