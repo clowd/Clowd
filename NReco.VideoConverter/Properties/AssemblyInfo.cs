29bc8d5e2fd5db7ba5c66c6759f260e7d4e9afa4
Caelan Sayler <caelan@tireacquire.com>
2015-11-11 02:52:19 +0000

    Add NReco source
    git-tfs-id: [https://caesay.visualstudio.com/DefaultCollection]$/Clowd;C65

---
+++ NReco.VideoConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Vitalii Fedorchenko")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 Vitalii Fedorchenko")]
+[assembly: AssemblyDescription("Video converter component for C# (.NET) based on FFMpeg utility. Can be used for converting video, transcoding live streams, extracting video thumbnails.")]
+[assembly: AssemblyFileVersion("1.0.7")]
+[assembly: AssemblyProduct("NReco.VideoConverter")]
+[assembly: AssemblyTitle("NReco.VideoConverter")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(false)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: Guid("ae5b6bbf-0dde-4ad9-8842-e8a157404f24")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
+
+[assembly: InternalsVisibleTo("Screeney")]
