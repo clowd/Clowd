6c72095b56a58f318aff5db1ae0f0d3d2371b97f
Caelan <git@caesay.com>
2016-02-25 19:06:49 -0700

    initial settings page commit
    Former-commit-id: 4aa35ab97f01cbf488f5c5cf9ad129ab8dfe3d39

---
+++ Clowd/Controls/KeyGestureEditor.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Clowd.Controls
+{
+    public class KeyGestureEditor : UserControl
+    {
+        public KeyGesture Gesture
+        {
+            get { return (KeyGesture)GetValue(GestureProperty); }
+            set { SetValue(GestureProperty, value); }
+        }
+
+        public static readonly DependencyProperty GestureProperty =
+            DependencyProperty.Register("Gesture", typeof(KeyGesture), typeof(KeyGestureEditor),
+                new PropertyMetadata(null, GestureChangedCallback));
+
+        public bool IsEditing { get; private set; }
+
+        private static void GestureChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var ths = (KeyGestureEditor)dependencyObject;
+            ths.UpdateControls();
+        }
+
+        private Button _button;
+        private Border _status;
+
+        public KeyGestureEditor()
+        {
+            var grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(10) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50) });
+
+            _button = new Button();
+            _button.Click += _button_Click;
+            _button.ToolTip = "Click to edit the current gesture";
+            _button.Style = null;
+            _button.FocusVisualStyle = null;
+            grid.Children.Add(_button);
+
+            _status = new Border();
+            _status.BorderBrush = Brushes.DarkGray;
+            _status.BorderThickness = new Thickness(1);
+            Grid.SetColumn(_status, 2);
+            grid.Children.Add(_status);
+
+            this.Content = grid;
+
+            UpdateControls();
+        }
+
+        private void _button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsEditing)
+                return;
+            IsEditing = true;
+            this.KeyDown += OnKeyDown;
+            this.KeyUp += OnKeyUp;
+            UpdateControls();
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            // this is because the PrtScr button only shows up in the KeyUp handler
+            Key key = (e.Key == Key.System ? e.SystemKey : e.Key);
+            if (key == Key.PrintScreen)
+                FinishEditing(key, Keyboard.Modifiers);
+            else
+                UpdateControls();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            Key key = (e.Key == Key.System ? e.SystemKey : e.Key);
+            var keyCode = (int)key;
+            // ignore any known modifier keys.
+            if (keyCode == 70 || keyCode == 71 || (keyCode >= 116 && keyCode <= 121))
+                UpdateControls();
+            else
+                FinishEditing(key, Keyboard.Modifiers);
+        }
+
+        private void FinishEditing(Key key, ModifierKeys modifiers)
+        {
+            IsEditing = false;
+            this.KeyDown -= OnKeyDown;
+            this.KeyUp -= OnKeyUp;
+            try
+            {
+                Gesture = new KeyGesture(key, modifiers);
+            }
+            catch
+            {
+                // invalid keygesture
+                Gesture = null;
+            }
+            UpdateControls();
+        }
+        private void UpdateControls()
+        {
+            if (IsEditing)
+            {
+                _status.Background = Brushes.PaleGoldenrod;
+                StringBuilder key = new StringBuilder();
+                foreach (var en in GetUniqueFlags(Keyboard.Modifiers))
+                {
+                    key.Append((ModifierKeys)en);
+                    key.Append('+');
+                }
+                key.Append(" ...");
+                _button.Content = key.ToString();
+            }
+            else
+            {
+                if (Gesture == null)
+                {
+                    _button.Content = "(undefined)";
+                    _status.Background = Brushes.PaleVioletRed;
+                }
+                else
+                {
+                    _button.Content = Gesture.GetDisplayStringForCulture(CultureInfo.CurrentUICulture);
+                    _status.Background = Brushes.PaleGreen;
+                }
+            }
+        }
+
+        private IEnumerable<Enum> GetUniqueFlags(Enum flags)
+        {
+            ulong flag = 1;
+            foreach (var value in Enum.GetValues(flags.GetType()).Cast<Enum>())
+            {
+                ulong bits = Convert.ToUInt64(value);
+                while (flag < bits)
+                {
+                    flag <<= 1;
+                }
+
+                if (flag == bits && flags.HasFlag(value))
+                {
+                    yield return value;
+                }
+            }
+        }
+    }
+}
---
+++ Clowd/SettingsPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Clowd.Controls;
+using PropertyChanged;
+using PropertyTools.Wpf;
+
+namespace Clowd
+{
+    [ImplementPropertyChanged]
+    public partial class SettingsPage : UserControl
+    {
+        public GeneralSettings SelectedItem { get; set; }
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class DisplayModeToPropertyTabVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (SettingsDisplayMode)value;
+            if (val == SettingsDisplayMode.SinglePage)
+                return PropertyTools.Wpf.TabVisibility.Collapsed;
+            else
+                return PropertyTools.Wpf.TabVisibility.VisibleIfMoreThanOne;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AppSettingsItemFactory : DefaultPropertyItemFactory
+    {
+        protected override IEnumerable<PropertyItem> CreatePropertyItems(object instance, IPropertyGridOptions options)
+        {
+            var instanceType = instance.GetType();
+            var properties = TypeDescriptor.GetProperties(instance);
+            foreach (PropertyDescriptor pd in properties)
+            {
+                if (options.ShowDeclaredOnly && pd.ComponentType != instanceType)
+                    continue;
+                if (!pd.IsBrowsable())
+                    continue;
+                if (!options.ShowReadOnlyProperties && pd.IsReadOnly())
+                    continue;
+                if (options.RequiredAttribute != null && pd.GetFirstAttributeOrDefault(options.RequiredAttribute) == null)
+                    continue;
+
+                ExpandAsCategoryAttribute expanded;
+                if (Type.GetTypeCode(pd.PropertyType) == TypeCode.Object
+                    && (expanded = pd.GetFirstAttributeOrDefault<ExpandAsCategoryAttribute>()) != null)
+                {
+                    foreach (var xProp in this.CreatePropertyItems(pd.GetValue(instance), options))
+                    {
+                        if (xProp.Category == "Misc")
+                        {
+                            xProp.Category = xProp.Tab = expanded.Category;
+                        }
+                        yield return xProp;
+                    }
+                }
+                else yield return this.CreatePropertyItem(pd, properties, instance);
+            }
+        }
+
+        public override PropertyItem CreatePropertyItem(PropertyDescriptor pd, PropertyDescriptorCollection propertyDescriptors, object instance)
+        {
+            var b = base.CreatePropertyItem(pd, propertyDescriptors, instance);
+            b.Tab = b.Category;
+            return b;
+        }
+    }
+
+    public class AppSettingsControlFactory : DefaultPropertyControlFactory
+    {
+        public override FrameworkElement CreateControl(PropertyItem property, PropertyControlFactoryOptions options)
+        {
+            if (property.Is(typeof(KeyGesture)))
+            {
+                var editor = new KeyGestureEditor();
+                editor.SetBinding(KeyGestureEditor.GestureProperty, property.CreateBinding());
+                return editor;
+            }
+
+            return base.CreateControl(property, options);
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExpandAsCategoryAttribute : Attribute
+    {
+        public virtual string Category { get; private set; }
+        public ExpandAsCategoryAttribute(string category)
+        {
+            Category = category;
+        }
+    }
+}
