a130cb2c1aec6f33e4569c17542da7257a694ed2
rstarkov <sourcecode@roman.st>
2016-02-28 17:33:51 +0000

    Correctly deserialize settings files with colors in the previous format.
    Former-commit-id: 2ab422be73d0af3adccf326dad92480bd23a1345

--- Clowd/Utilities/ClassifyOptions.cs
+++ Clowd/Utilities/ClassifyOptions.cs
@@ -1,33 +1,63 @@
 ﻿using RT.Util.Serialization;
-using System;
 using System.Globalization;
 using System.Windows.Media;
+using System.Xml.Linq;
 
 namespace Clowd.Utilities
 {
     /// <summary>
     /// Enables <see cref="Classify"/> to save color properties as strings of a human-editable form.
     /// </summary>
-    sealed class ClassifyColorTypeOptions : ClassifyTypeOptions,
-        IClassifySubstitute<Color, string>
+    sealed class ClassifyColorTypeOptions : ClassifyTypeOptions, IClassifyXmlTypeProcessor, IClassifySubstitute<Color, string>
     {
-        Color IClassifySubstitute<Color, string>.FromSubstitute(string instance) { return FromSubstituteD(instance); }
-        private Color FromSubstituteD(string instance)
+        public void AfterDeserialize(object obj, XElement element)
         {
+        }
+
+        public void AfterSerialize(object obj, XElement element)
+        {
+        }
+
+        public void BeforeDeserialize(XElement element)
+        {
+            Color dummy;
+            if (!fromSubstitute(element.Value, out dummy))
+            {
+                // then it's using the old, native format: deserialize that and replace the value in the xml (to be processed via FromSubstitute later)
+                var color = ClassifyXml.Deserialize<Color>(element, new ClassifyOptions());
+                element.Value = ToSubstitute(color);
+            }
+        }
+
+        public void BeforeSerialize(object obj)
+        {
+        }
+
+        private bool fromSubstitute(string instance, out Color color)
+        {
+            color = Colors.Black;
             try
             {
                 if (!instance.StartsWith("#") || (instance.Length != 7 && instance.Length != 9))
-                    throw new Exception();
+                    return false;
                 int alpha = instance.Length == 7 ? 255 : int.Parse(instance.Substring(1, 2), NumberStyles.HexNumber);
                 int r = int.Parse(instance.Substring(instance.Length == 7 ? 1 : 3, 2), NumberStyles.HexNumber);
                 int g = int.Parse(instance.Substring(instance.Length == 7 ? 3 : 5, 2), NumberStyles.HexNumber);
                 int b = int.Parse(instance.Substring(instance.Length == 7 ? 5 : 7, 2), NumberStyles.HexNumber);
-                return Color.FromArgb((byte)alpha, (byte)r, (byte)g, (byte)b);
+                color = Color.FromArgb((byte)alpha, (byte)r, (byte)g, (byte)b);
+                return true;
             }
             catch
             {
-                return Colors.Black;
+                return false;
             }
+        }
+
+        public Color FromSubstitute(string instance)
+        {
+            Color result;
+            fromSubstitute(instance, out result);
+            return result;
         }
 
         public string ToSubstitute(Color instance)
