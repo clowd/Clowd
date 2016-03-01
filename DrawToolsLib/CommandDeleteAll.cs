7ea3c1263849dc577329b8583fbfc38b669125b8
Caelan Sayler <caelantsayler@gmail.com>
2016-02-29 20:11:41 -0700

    misc fixes, general clean up. part 1
    Former-commit-id: a232884574d48eab39bcfb5d49f9d59293d12980

--- DrawToolsLib/CommandDeleteAll.cs
+++ DrawToolsLib/CommandDeleteAll.cs
@@ -1,48 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-
-
+using System.Linq;
 
 namespace DrawToolsLib
 {
-    /// <summary>
-    /// Delete command.
-    /// </summary>
-    class CommandDeleteAll : CommandBase
+    internal class CommandDeleteAll : CommandBase
     {
-        List<PropertiesGraphicsBase> cloneList;
+        private readonly PropertiesGraphicsBase[] _cloneList;
 
         // Create this command BEFORE applying Delete All function.
         public CommandDeleteAll(DrawingCanvas drawingCanvas)
         {
-            cloneList = new List<PropertiesGraphicsBase>();
-
-            // Make clone of the whole list.
-            foreach(GraphicsBase g in drawingCanvas.GraphicsList)
-            {
-                cloneList.Add(g.CreateSerializedObject());
-            }
+            _cloneList = drawingCanvas.GraphicsList
+                .OfType<GraphicsBase>()
+                .Select(g => g.CreateSerializedObject())
+                .ToArray();
         }
 
-        /// <summary>
-        /// Add all deleted objects to GraphicsList
-        /// </summary>
         public override void Undo(DrawingCanvas drawingCanvas)
         {
-            foreach (PropertiesGraphicsBase o in cloneList)
+            foreach (PropertiesGraphicsBase o in _cloneList)
             {
                 drawingCanvas.GraphicsList.Add(o.CreateGraphics());
             }
         }
 
-        /// <summary>
-        /// Detete All again
-        /// </summary>
         public override void Redo(DrawingCanvas drawingCanvas)
         {
             drawingCanvas.GraphicsList.Clear();
