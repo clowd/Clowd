4cf6d59ca3367548e5b6c2009bafcec122f17ea9
Caelan Sayler <caelantsayler@gmail.com>
2016-03-08 17:23:32 -0700

    add script compiler project and some initial code
    Former-commit-id: 6ca81e82f9674599f7934abafa65d3fe1cdefa67

---
+++ ScriptLib/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge;
+using Bridge.Html5;
+
+namespace ScriptLib
+{
+    public static class Extensions
+    {
+        public static void SaveAs(this Blob data, string filename)
+        {
+            Script.Call("saveAs", data, filename);
+        }
+    }
+}
