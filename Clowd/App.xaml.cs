clowd/Clowd

commit ba693acb259d9ac05d57d517b260849a45936ec8

When using the QuickCapture feature (full screen or current window), show the upload/photo/etc. options instead of immediately uploading.

Clowd/App.xaml.cs
@@ -1,9 +1,14 @@
-﻿using Clowd.Utilities;
+﻿using Clowd.Interop;
+using Clowd.Utilities;
 using Exceptionless;
 using Exceptionless.Logging;
+using Exceptionless.Plugins;
 using Ionic.Zip;
 using NotifyIconLib;
+using RT.Util;
 using RT.Util.ExtensionMethods;
+using RT.Util.Serialization;
+using ScreenVersusWpf;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,25 +17,15 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Reflection;
-using System.Runtime.Remoting.Channels;
 using System.ServiceModel;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
-using CS.Wpf;
-using Exceptionless.Dependency;
-using Exceptionless.Plugins;
-using RT.Util;
-using RT.Util.Serialization;
 using TaskDialogInterop;
 using Color = System.Windows.Media.Color;
-using GlobalTrigger = Clowd.Utilities.GlobalTrigger;
 using Point = System.Windows.Point;
 
 namespace Clowd
@@ -603,12 +598,13 @@ namespace Clowd
                 OnCommandLineArgsReceived(this, new CommandLineEventArgs(_args));
             }
         }
-        public async void StartCapture()
+
+        public async void StartCapture(ScreenRect? region = null)
         {
             if (_prtscrWindowOpen)
                 return;
 
-            var wnd = await CaptureWindow.ShowNew();
+            var wnd = await CaptureWindow.ShowNew(region);
             wnd.Closed += (s, e) =>
             {
                 _prtscrWindowOpen = false;
@@ -616,20 +612,18 @@ namespace Clowd
             _prtscrWindowOpen = true;
         }
 
-        public void QuickCapture(bool fullscreen = true)
+        public void QuickCaptureFullScreen()
         {
-            if (!_initialized)
-                return;
-
-            using (MemoryStream ms = new MemoryStream())
-            using (var source = fullscreen
-                ? ScreenUtil.Capture(captureCursor: Settings.CaptureSettings.ScreenshotWithCursor)
-                : ScreenUtil.CaptureActiveWindow())
-            {
-                source.Save(ms, ImageFormat.Png);
-                var task = UploadManager.Upload(ms.ToArray(), "clowd-default.png");
-            }
+            StartCapture(ScreenTools.VirtualScreen.Bounds);
         }
+
+        public void QuickCaptureCurrentWindow()
+        {
+            var foreground = USER32.GetForegroundWindow();
+            var bounds = USER32EX.GetWindowRectangle(foreground);
+            StartCapture(ScreenRect.FromSystem(bounds));
+        }
+
         public void UploadFile(Window owner = null)
         {
             if (!_initialized)
@@ -648,7 +642,7 @@ namespace Clowd
                 {
                     ShowActivated = false,
                     Opacity = 0,
-                    WindowStyle = WindowStyle.None,
+                    WindowStyle = System.Windows.WindowStyle.None,
                     ResizeMode = ResizeMode.NoResize,
                     AllowsTransparency = true,
                     Width = 1,

Clowd/CaptureWindow.xaml.cs
@@ -33,9 +33,11 @@ namespace Clowd
         private bool draggingArea = false;
         private ScreenPoint draggingOrigin = default(ScreenPoint);
         private WindowFinder2 windowFinder = new WindowFinder2();
+        private ScreenRect? initialRegion = null;
 
-        private CaptureWindow()
+        private CaptureWindow(ScreenRect? initial = null)
         {
+            initialRegion = initial;
             InitializeComponent();
             this.SourceInitialized += CaptureWindow_SourceInitialized;
             this.Loaded += CaptureWindow_Loaded;
@@ -75,9 +77,9 @@ namespace Clowd
             g.Dispose();
             return newBitmap;
         }
-        public static async Task<CaptureWindow> ShowNew()
+        public static async Task<CaptureWindow> ShowNew(ScreenRect? captureRegion = null)
         {
-            var c = new CaptureWindow();
+            var c = new CaptureWindow(captureRegion);
             if (TaskWindow.Current?.IsVisible == true)
             {
                 await TaskWindow.Current.Hide();
@@ -124,8 +126,17 @@ namespace Clowd
             if (!System.Diagnostics.Debugger.IsAttached)
                 Interop.USER32.SetWindowPos(this.Handle, 0, 0, 0, 0, 0,
                     Interop.SWP.NOMOVE | Interop.SWP.NOSIZE | Interop.SWP.SHOWWINDOW);
-            UpdateCanvasMode(true);
+            if (initialRegion == null)
+            {
+                UpdateCanvasMode(true);
+            }
+            else
+            {
+                UpdateCanvasMode(false);
+                UpdateCanvasSelection(initialRegion.Value);
+            }
         }
+
         private void CaptureWindow_SourceInitialized(object sender, EventArgs e)
         {
             this.Handle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
@@ -394,7 +405,7 @@ namespace Clowd
             {
                 var wfMouse = System.Windows.Forms.Cursor.Position;
                 ShowTips = false;
-                (crosshair).CaptureMouse();
+                crosshair.CaptureMouse();
                 draggingArea = true;
                 draggingOrigin = ScreenTools.GetMousePosition();
             }
@@ -456,7 +467,7 @@ namespace Clowd
         {
             if (!draggingArea)
                 return;
-            (crosshair).ReleaseMouseCapture();
+            crosshair.ReleaseMouseCapture();
             draggingArea = false;
 
             UpdateCanvasMode(false);
@@ -497,8 +508,8 @@ namespace Clowd
                 {
                     ManageCanvasMouseHandlers(false);
                     ManageSelectionResizeHandlers(true);
-                    capturing = false;
                 }
+                capturing = false;
                 areaSizeIndicator.Visibility = Visibility.Hidden;
                 pixelMagnifier.Visibility = Visibility.Hidden;
                 crosshair.Visibility = Visibility.Collapsed;

Clowd/Settings.cs
@@ -131,11 +131,11 @@ namespace Clowd
 
         [Category("Hotkeys"), DisplayName("Capture - Fullscreen"), ClassifyIgnoreIfDefault]
         public GlobalTrigger CaptureFullscreenShortcut { get; set; }
-            = new GlobalTrigger(Key.PrintScreen, ModifierKeys.Control, () => App.Current.QuickCapture(true));
+            = new GlobalTrigger(Key.PrintScreen, ModifierKeys.Control, App.Current.QuickCaptureFullScreen);
 
         [Category("Hotkeys"), DisplayName("Capture - Active Window"), ClassifyIgnoreIfDefault]
         public GlobalTrigger CaptureActiveShortcut { get; set; }
-            = new GlobalTrigger(Key.PrintScreen, ModifierKeys.Alt, () => App.Current.QuickCapture(false));
+            = new GlobalTrigger(Key.PrintScreen, ModifierKeys.Alt, App.Current.QuickCaptureCurrentWindow);
     }
 
     [ImplementPropertyChanged]

Clowd/Utilities/ScreenUtil.cs
@@ -26,14 +26,14 @@ namespace Clowd.Utilities
             return bitmap;
         }
 
-        public static Bitmap CaptureActiveWindow()
+        public static Bitmap CaptureActiveWindow(bool captureCursor = false)
         {
             var foreground = USER32.GetForegroundWindow();
             var bounds = USER32EX.GetWindowRectangle(foreground);
-            return Capture(ScreenRect.FromSystem(bounds), false);
+            return Capture(ScreenRect.FromSystem(bounds), captureCursor);
         }
 
-        private static void DrawCursor(Graphics g, System.Drawing.Point origin)
+        private static void DrawCursor(Graphics g, Point origin)
         {
             CURSORINFO cursorInfo;
             cursorInfo.cbSize = Marshal.SizeOf(typeof(CURSORINFO));

commit 2b8830ccf3008aaebb194f8dd26551c731eddc84

Add using to fix release builds, broken in ba693ac

Clowd/App.xaml.cs
@@ -24,6 +24,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
+using Exceptionless.Dependency;
 using TaskDialogInterop;
 using Color = System.Windows.Media.Color;
 using Point = System.Windows.Point;