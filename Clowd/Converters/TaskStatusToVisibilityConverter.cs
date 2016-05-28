caaccc26460489f0b0bdde70ca0c6bcc124972f4
Timwi <github@timwi.de>
2016-05-29 00:10:34 +0200

    Change TaskStatusToVisibilityConverter parameter meaning

--- Clowd/Converters/TaskStatusToVisibilityConverter.cs
+++ Clowd/Converters/TaskStatusToVisibilityConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -12,13 +13,9 @@ namespace Clowd.Converters
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (parameter.Equals("animation"))
-                return value.Equals(TaskViewItem.TaskStatus.Waiting) || value.Equals(TaskViewItem.TaskStatus.InProgress) ? Visibility.Visible : Visibility.Collapsed;
-            else if (parameter.Equals("checkmark"))
-                return value.Equals(TaskViewItem.TaskStatus.Complete) || value.Equals(TaskViewItem.TaskStatus.Executed) ? Visibility.Visible : Visibility.Collapsed;
-            else if (parameter.Equals("cross"))
-                return value.Equals(TaskViewItem.TaskStatus.Canceled) || value.Equals(TaskViewItem.TaskStatus.Error) ? Visibility.Visible : Visibility.Collapsed;
-            return Visibility.Visible;
+            var visibleStates = ((string)parameter).Split(';');
+            var taskStatus = value as TaskViewItem.TaskStatus?;
+            return taskStatus != null && visibleStates.Contains(taskStatus.Value.ToString()) ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
