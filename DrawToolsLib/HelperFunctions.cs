258449e63ee4d8f2f8dadbe96ca6e815c262262e
Caelan <git@caesay.com>
2016-05-30 22:56:28 -0600

    Removed unused code
    Former-commit-id: c464929dcc45120edeb53e474b0477f88d13c5d4

--- DrawToolsLib/HelperFunctions.cs
+++ DrawToolsLib/HelperFunctions.cs
@@ -10,55 +10,9 @@ using DrawToolsLib.Graphics;
 
 namespace DrawToolsLib
 {
-
     internal static class HelperFunctions
     {
         public static Cursor DefaultCursor => Cursors.Arrow;
-
-        public static bool ApplyProperty<T>(DrawingCanvas canvas, Func<T, bool> action, bool addToHistory, bool selectedOnly)
-            where T : GraphicsBase
-        {
-            CommandChangeState command = new CommandChangeState(canvas);
-            bool wasChange = false;
-            foreach (GraphicsBase g in canvas.Selection)
-            {
-                if (g.IsSelected || selectedOnly)
-                {
-                    T gt = g as T;
-                    if (gt != null)
-                    {
-                        wasChange = action(gt);
-                    }
-                }
-            }
-
-            if (wasChange && addToHistory)
-            {
-                command.NewState(canvas);
-                canvas.AddCommandToHistory(command);
-            }
-            return wasChange;
-        }
-        public static bool ApplyFontFamily(DrawingCanvas drawingCanvas, string value, bool addToHistory)
-        {
-            return false;
-        }
-        public static bool ApplyFontStyle(DrawingCanvas drawingCanvas, FontStyle value, bool addToHistory)
-        {
-            return false;
-        }
-        public static bool ApplyFontWeight(DrawingCanvas drawingCanvas, FontWeight value, bool addToHistory)
-        {
-            return false;
-        }
-        public static bool ApplyFontStretch(DrawingCanvas drawingCanvas, FontStretch value, bool addToHistory)
-        {
-            return false;
-        }
-        public static bool ApplyFontSize(DrawingCanvas drawingCanvas, double value, bool addToHistory)
-        {
-            return false;
-        }
 
         public static Rect CreateRectSafe(double Left, double Top, double Right, double Bottom)
         {
