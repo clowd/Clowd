bbdb5bc29d911567829e2eea894e9f174d6e7a29
Caelan Sayler <caelantsayler@gmail.com>
2016-06-04 22:30:02 -0600

    Still wasn't disposing everything when creating cursors.
    Former-commit-id: eb80a9e762309c298d608c9206e366e026673899

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.1.82")]
-[assembly: AssemblyFileVersion("2.3.1.82")]
+[assembly: AssemblyVersion("2.3.1.83")]
+[assembly: AssemblyFileVersion("2.3.1.83")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
