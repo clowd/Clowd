c9a891b0b9799b7d907df23da321265e712fd3c3
Caelan Sayler <caelantsayler@gmail.com>
2016-06-16 16:00:32 -0600

    Update CS.WPF / CS.Util
    Former-commit-id: eaf994f451ee51e2e7346a83c6707a99ededf48d

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.1.90")]
-[assembly: AssemblyFileVersion("2.3.1.90")]
+[assembly: AssemblyVersion("2.3.3.92")]
+[assembly: AssemblyFileVersion("2.3.3.92")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
