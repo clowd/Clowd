f8337b2fea34a8ba1b200b77eff6babe0840dc1a
Caelan <git@caesay.com>
2017-10-31 19:36:58 +1100

    Remove video code not being used anyways
    Former-commit-id: 1ef9b69f443920320e28500f2cc1baf413a00d9e

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.6.128")]
-[assembly: AssemblyFileVersion("2.3.6.128")]
+[assembly: AssemblyVersion("2.3.6.129")]
+[assembly: AssemblyFileVersion("2.3.6.129")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
