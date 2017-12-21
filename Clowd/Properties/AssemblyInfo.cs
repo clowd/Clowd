ba49d0e7d67e7a40acd71b9104c5c47f02af35ba
Caelan Sayler <caelantsayler@gmail.com>
2017-12-21 18:29:01 +1100

    Direct uploads to azure
    Former-commit-id: 67706c066e2e1f8c6f594e47e1fc1be4b2ef9782

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.6.143")]
-[assembly: AssemblyFileVersion("2.3.6.143")]
+[assembly: AssemblyVersion("2.3.6.152")]
+[assembly: AssemblyFileVersion("2.3.6.152")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
