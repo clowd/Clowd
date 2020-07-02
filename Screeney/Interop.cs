6657eb796e861c9550b83d27b434e1a09ded5386
Caelan <git@caesay.com>
2020-07-02 20:16:42 +0100

    WIP: Add Screeney
    Former-commit-id: c34b855a07b01f76e5833efe8f8dcaecb508f737

---
+++ Screeney/Interop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screeney
+{
+    public static class Interop
+    {
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy",
+            CallingConvention = CallingConvention.Cdecl,
+            SetLastError = false)]
+        public static extern IntPtr memcpy(IntPtr dest, IntPtr src, UIntPtr count);
+    }
+}
---
+++ Screeney/Video/MonitorUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Screeney.Video
+{
+    class MonitorUtil
+    {
+        public static Screen GetScreenContainingRect(Rectangle bounds)
+        {
+            var point = new System.Drawing.Point((int)(bounds.Left + bounds.Width / 2), (int)(bounds.Top + bounds.Height / 2));
+            Screen[] allScreens = Screen.AllScreens;
+            for (int i = 0; i < (int)allScreens.Length; i++)
+            {
+                Screen screen = allScreens[i];
+                if (screen.Bounds.Contains(point))
+                {
+                    return screen;
+                }
+            }
+            return null;
+        }
+    }
+}
