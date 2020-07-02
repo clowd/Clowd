265c33b24dfdcdf41810de51371e2aaf27cafdcc
Caelan Sayler <git@caesay.com>
2020-07-02 14:09:02 +0100

    WIP bring in old installer features to settings page
    Former-commit-id: 60bc6da54a43e9ae48fff7807b7c17ccb1e3a03a

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.23.201")]
-[assembly: AssemblyFileVersion("2.3.23.201")]
+[assembly: AssemblyVersion("2.3.23.214")]
+[assembly: AssemblyFileVersion("2.3.23.214")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
