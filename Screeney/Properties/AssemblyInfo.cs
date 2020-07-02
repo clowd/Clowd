c34b855a07b01f76e5833efe8f8dcaecb508f737
Caelan Sayler <git@caesay.com>
2020-07-02 20:16:42 +0100

    WIP: Add Screeney

---
+++ Screeney/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Screeney")]
+[assembly: AssemblyDescription("Screen capture library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Caesa Consulting Ltd.")]
+[assembly: AssemblyProduct("Screeney")]
+[assembly: AssemblyCopyright("Copyright © Caesa Consulting Ltd.  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("af84f83b-28c3-4298-b388-c31db95a6790")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
