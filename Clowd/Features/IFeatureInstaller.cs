60bc6da54a43e9ae48fff7807b7c17ccb1e3a03a
Caelan Sayler <git@caesay.com>
2020-07-02 14:09:02 +0100

    WIP bring in old installer features to settings page

---
+++ Clowd/Features/IFeatureInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowd.Installer.Features
+{
+    public interface IFeatureInstaller
+    {
+
+        bool CheckInstalled(string assetPath, RegistryQuery context);
+        void Install(string assetPath, InstallMode context);
+        void Uninstall(string assetPath, RegistryQuery context);
+    }
+}
