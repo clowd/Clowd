e6617817515e0bea1834739e5ab58a1425c5cdd6
Caelan Sayler <git@caesay.com>
2020-07-14 09:45:25 +0100

    Add FlagsHelper class to Interop

---
+++ Clowd.Interop/FlagsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowd.Interop
+{
+    public static class FlagsHelper
+    {
+        public static bool IsSet<T>(T flags, T flag) where T : struct
+        {
+            int flagsValue = (int)(object)flags;
+            int flagValue = (int)(object)flag;
+            return (flagsValue & flagValue) == flagValue;
+        }
+        public static void Set<T>(ref T flags, T flag) where T : struct
+        {
+            int flagsValue = (int)(object)flags;
+            int flagValue = (int)(object)flag;
+            flags = (T)(object)(flagsValue | flagValue);
+        }
+        public static void Unset<T>(ref T flags, T flag) where T : struct
+        {
+            int flagsValue = (int)(object)flags;
+            int flagValue = (int)(object)flag;
+            flags = (T)(object)(flagsValue & (~flagValue));
+        }
+
+    }
+}
