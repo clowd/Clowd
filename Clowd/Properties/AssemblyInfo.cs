82e8c9c3aa9ffae762d953b5536c6b45c14c53d0
Caelan Sayler <git@caesay.com>
2020-07-14 17:26:22 +0100

    WIP new screen recorder using ffmpeg directly

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.25.358")]
-[assembly: AssemblyFileVersion("2.3.25.358")]
+[assembly: AssemblyVersion("2.3.25.371")]
+[assembly: AssemblyFileVersion("2.3.25.371")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
