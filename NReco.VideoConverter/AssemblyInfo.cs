1ad547329b5277c044af985fa20effe7b0f43c44
Caelan <git@caesay.com>
2020-07-14 17:24:18 +0100

    Add new version of NReco
    Former-commit-id: 02477939605168ab51ce01c93d04a85951dc0f32

---
+++ NReco.VideoConverter/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: Guid("ae5b6bbf-0dde-4ad9-8842-e8a157404f24")]
+[assembly: AssemblyCompany("Vitalii Fedorchenko")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Video converter component for C#/.NET (FFMpeg wrapper). Can convert video files, transcode live streams, extract video thumbnails, apply watermarks to video, screen capture etc.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("NReco.VideoConverter")]
+[assembly: AssemblyProduct("NReco.VideoConverter")]
+[assembly: AssemblyCopyright("Copyright © 2013-2016 Vitalii Fedorchenko")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.1.2.0")]
---
+++ NReco.VideoConverter/ConvertProgressEventArgs.cs
@@ -0,0 +1,26 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: NReco.VideoConverter.ConvertProgressEventArgs
+// Assembly: NReco.VideoConverter, Version=1.1.2.0, Culture=neutral, PublicKeyToken=395ccb334978a0cd
+// MVID: 503557EA-2300-465E-B59C-87E1183E58F6
+// Assembly location: C:\Users\Caelan\Downloads\video_converter_free\NReco.VideoConverter.dll
+
+using System;
+
+namespace NReco.VideoConverter
+{
+  /// <summary>Provides data for ConvertProgress event</summary>
+  public class ConvertProgressEventArgs : EventArgs
+  {
+    /// <summary>Total media stream duration</summary>
+    public TimeSpan TotalDuration { get; private set; }
+
+    /// <summary>Processed media stream duration</summary>
+    public TimeSpan Processed { get; private set; }
+
+    public ConvertProgressEventArgs(TimeSpan processed, TimeSpan totalDuration)
+    {
+      this.TotalDuration = totalDuration;
+      this.Processed = processed;
+    }
+  }
+}
