8ed533d1b46c26e4c31285f8ce82211f9c43ea69
Roman Starkov <sourcecode@roman.st>
2020-07-17 20:51:47 +0100

    Fix AssemblyInfo incrementer
    Former-commit-id: e6375aec0e36b85a68bebc00de26db3b75595a30

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.25.504")]
-[assembly: AssemblyFileVersion("2.3.25.504")]
+[assembly: AssemblyVersion("2.3.25.505")]
+[assembly: AssemblyFileVersion("2.3.25.505")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
