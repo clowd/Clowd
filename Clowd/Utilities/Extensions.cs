4cb74978e19441983e1e534ee92fcf8f6ae35f3d
Caelan Sayler <git@caesay.com>
2020-07-22 19:30:11 +0100

    Faster Bitmap -> BitmapSource
    Former-commit-id: 15f8b735b2f9bf9ec51ddcbe33e75c7b394f32ce

--- Clowd/Utilities/Extensions.cs
+++ Clowd/Utilities/Extensions.cs
@@ -76,6 +76,21 @@ namespace Clowd
             }
             return bs;
         }
+        public static BitmapSource ConvertToBitmapSourceFast(this System.Drawing.Bitmap bitmap)
+        {
+            var bitmapData = bitmap.LockBits(
+                new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+            var bitmapSource = BitmapSource.Create(
+                bitmapData.Width, bitmapData.Height,
+                bitmap.HorizontalResolution, bitmap.VerticalResolution,
+                PixelFormats.Bgr24, null,
+                bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+            bitmap.UnlockBits(bitmapData);
+            return bitmapSource;
+        }
 
         public static System.Drawing.Bitmap Crop(this System.Drawing.Bitmap b, System.Drawing.Rectangle r)
         {
