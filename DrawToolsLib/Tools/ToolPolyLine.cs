ae0265b4c529194417f965eb1c78d8fff68992a8
Caelan Sayler <git@caesay.com>
2020-07-22 16:58:58 +0100

    New Pencil / PolyLine Graphic. Looks nicer, is a vector and is faster to draw

--- DrawToolsLib/Tools/ToolPolyLine.cs
+++ DrawToolsLib/Tools/ToolPolyLine.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -19,14 +19,11 @@ namespace DrawToolsLib.Tools
 
         public override void OnMouseDown(DrawingCanvas drawingCanvas, MouseButtonEventArgs e)
         {
+            base.OnMouseDown(drawingCanvas, e);
+
             Point p = e.GetPosition(drawingCanvas);
-
             newPolyLine = new GraphicPolyLine(drawingCanvas, new Point(p.X + 1, p.Y + 1));
-
-            drawingCanvas.UnselectAll();
-            newPolyLine.IsSelected = true;
             drawingCanvas.GraphicsList.Add(newPolyLine);
-            drawingCanvas.CaptureMouse();
         }
 
         public override void OnMouseMove(DrawingCanvas drawingCanvas, MouseEventArgs e)
@@ -40,24 +37,17 @@ namespace DrawToolsLib.Tools
 
         public override void OnMouseUp(DrawingCanvas drawingCanvas, MouseButtonEventArgs e)
         {
-            newPolyLine.FinishDrawing();
-            newPolyLine.IsSelected = false;
-
-            var visual = new DrawingVisual();
-            using (var context = visual.RenderOpen())
-            {
-                context.PushTransform(new TranslateTransform(-newPolyLine.Bounds.X, -newPolyLine.Bounds.Y));
-                newPolyLine.Draw(context);
-            }
-
-            var bitmap = new RenderTargetBitmap((int)newPolyLine.Bounds.Width, (int)newPolyLine.Bounds.Height, 96, 96, PixelFormats.Default);
-            bitmap.Render(visual);
-
-            drawingCanvas.GraphicsList.RemoveAt(drawingCanvas.GraphicsList.Count - 1);
-            drawingCanvas.AddGraphic(new GraphicImage(drawingCanvas, newPolyLine.Bounds, bitmap) { IsSelected = true });
-
-            newPolyLine = null;
             base.OnMouseUp(drawingCanvas, e);
+
+            Point p = e.GetPosition(drawingCanvas);
+
+            newPolyLine.AddPoint(p);
+            newPolyLine.IsSelected = true;
+            newPolyLine.FinishDrawing();
+            newPolyLine.Normalize();
+
+            drawingCanvas.AddCommandToHistory(new CommandAdd(newPolyLine));
+            newPolyLine = null;
         }
     }
 }
