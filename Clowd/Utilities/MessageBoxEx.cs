ce68e5e62aa048cb09fa7eb396e61d7580767424
Caelan <git@caesay.com>
2020-08-07 23:27:27 +0100

    New message box utility class
    Former-commit-id: c3f42fe99cbb0275a86e507263a0063b0ec115f0

---
+++ Clowd/Utilities/MessageBoxEx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Ookii.Dialogs.Wpf;
+
+namespace Clowd.Utilities
+{
+    public class MessageBoxEx
+    {
+        public static bool ShowAction(
+            Window wnd,
+            string instruction,
+            string content,
+            TaskDialogIcon icon,
+            string trueBtnTxt,
+            string falseBtnTxt = "Close")
+        {
+            using (var dialog = new TaskDialog())
+            {
+                dialog.WindowTitle = App.ClowdAppName;
+                dialog.MainInstruction = instruction;
+                dialog.Content = content;
+                dialog.MainIcon = icon;
+
+                var trueBtn = new TaskDialogButton(trueBtnTxt);
+                var falseBtn = new TaskDialogButton(falseBtnTxt);
+
+                dialog.Buttons.Add(trueBtn);
+                dialog.Buttons.Add(falseBtn);
+
+                TaskDialogButton result;
+
+                if (wnd != null)
+                {
+                    result = dialog.ShowDialog(wnd);
+                }
+                else
+                {
+                    result = dialog.Show();
+                }
+
+                return result == trueBtn;
+            }
+        }
+    }
+}
