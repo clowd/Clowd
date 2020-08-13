08ffdc7367a41aed6e61c6b7b1969ea721f43c59
Caelan Sayler <git@caesay.com>
2020-08-13 12:46:44 +0100

    Initially add COM library back..
    Former-commit-id: a9f5b0d1fb974a2f2f2736df0e1a26f93cfd4ffd

---
+++ Clowd.Com/Audio/AudioCaptureFilter.cs
@@ -0,0 +1,70 @@
+﻿using DirectShow;
+using DirectShow.BaseClasses;
+using Sonic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clowd.Com
+{
+    [ComVisible(true)]
+    [Guid("98957b9c-71c2-46b7-95a9-eec80d9317e7")]
+    [AMovieSetup(Merit.Normal, AMovieSetup.CLSID_AudioInputDeviceCategory)]
+    class AudioCaptureFilter : BaseSourceFilter, IAMFilterMiscFlags
+    {
+        public const string FRIENDLY_NAME = "clowd-audio";
+        public AudioCaptureFilter() : base(FRIENDLY_NAME)
+        {
+            AddPin(new AudioCaptureStream("Capture", this));
+
+        }
+        protected override int OnInitializePins()
+        {
+            return NOERROR;
+        }
+
+        public override int Pause()
+        {
+            return base.Pause();
+        }
+
+        public override int Stop()
+        {
+            return base.Stop();
+        }
+
+        public override int GetState(int dwMilliSecsTimeout, out FilterState filtState)
+        {
+            return base.GetState(dwMilliSecsTimeout, out filtState);
+        }
+
+        [ComVisible(false)]
+        public class AudioCaptureStream : SourceStream
+        {
+            public AudioCaptureStream(string _name, BaseSourceFilter _filter)
+                : base(_name, _filter)
+            {
+
+            }
+
+            public override int DecideBufferSize(ref IMemAllocatorImpl pAlloc, ref AllocatorProperties prop)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override int FillBuffer(ref IMediaSampleImpl _sample)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int GetMiscFlags()
+        {
+            return (int)AMFilterMiscFlags.IsSource;
+        }
+    }
+}
---
+++ Clowd.Com/Interop.cs
@@ -0,0 +1,141 @@
+﻿using DirectShow;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Clowd.Com
+{
+    internal class USER32
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorInfo(out CURSORINFO pci);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CopyIcon(IntPtr hIcon);
+
+        [DllImport("user32.dll")]
+        public static extern bool DrawIcon(IntPtr hdc, int x, int y, IntPtr hIcon);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetIconInfo(IntPtr hIcon, out ICONINFO piconinfo);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("USER32.dll")]
+        public static extern short GetKeyState(VirtualKeyStates nVirtKey);
+
+        [DllImport("user32.dll")]
+        public static extern bool RedrawWindow(IntPtr hWnd, [In] ref RECT lprcUpdate, IntPtr hrgnUpdate, RedrawWindowFlags flags);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CURSORINFO
+        {
+            public int cbSize;
+            public int flags;
+            public IntPtr hCursor;
+            public POINT ptScreenPos;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.x, point.y);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ICONINFO
+        {
+            public bool fIcon;
+            public int xHotspot;
+            public int yHotspot;
+            public IntPtr hbmMask;
+            public IntPtr hbmColor;
+        }
+
+        [Flags]
+        public enum VirtualKeyStates : int
+        {
+            VK_LBUTTON = 0x01,
+            VK_RBUTTON = 0x02,
+        }
+
+        [Flags]
+        public enum RedrawWindowFlags : uint
+        {
+            Invalidate = 0x1,
+            UpdateNow = 0x100,
+        }
+    }
+    internal class GDI32
+    {
+        public const int SRCCOPY = 0x00CC0020;
+        public const int CAPTUREBLT = 0x40000000;
+        public const int SRCINVERT = 0x00660046;
+
+        [DllImport("gdi32.dll")]
+        public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hObjectSource, int nXSrc, int nYSrc, int dwRop);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteDC(IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+
+        [DllImport("gdi32.dll")]
+        public static extern int GetDIBits(IntPtr hdc, IntPtr hbmp, uint uStartScan, uint cScanLines, [Out] IntPtr lpvBits, ref BitmapInfo lpbmi, uint uUsage);
+
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BitmapInfo
+        {
+            public BitmapInfoHeader bmiHeader;
+            public int[] bmiColors;
+        }
+    }
+}
