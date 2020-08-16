e0a4040321937918d6a427ee42bd5589c6ba4655
Caelan <git@caesay.com>
2020-08-16 10:21:30 +0100

    Add UScreenCapture; DirectShow feature can now uninstall itself & adds control panel entry
    Former-commit-id: e672dd21bd2e59f53cc2f4efc53c634802caddfb

--- Clowd.Installer/Features/DShowFilter.cs
+++ Clowd.Installer/Features/DShowFilter.cs
@@ -10,17 +10,19 @@ namespace Clowd.Installer.Features
 {
     public class DShowFilter : IFeature
     {
-        private string[] _filters = new string[]
+        public string InstallDirectory => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), Constants.DirectShowAppName);
+
+        public string[] FilterNames => new string[]
         {
             "loopback-audio-x86.dll",
-            "loopback-audio-x64.dll"
+            "loopback-audio-x64.dll",
+            "UScreenCapture-x86.ax",
+            "UScreenCapture-x64.ax",
         };
-
-        private string _installDirectory => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), Constants.AppName + "DirectShow");
 
         public bool CheckInstalled(string assetPath)
         {
-            return Directory.Exists(_installDirectory) && Directory.EnumerateFiles(_installDirectory).Any();
+            return Directory.Exists(InstallDirectory) && Directory.EnumerateFiles(InstallDirectory).Any();
         }
 
         public void Install(string assetPath)
@@ -31,22 +33,65 @@ namespace Clowd.Installer.Features
                 return;
             }
 
-            if (!Directory.Exists(_installDirectory))
-                Directory.CreateDirectory(_installDirectory);
+            if (!Directory.Exists(InstallDirectory))
+                Directory.CreateDirectory(InstallDirectory);
 
-            foreach (var f in _filters)
+            StringBuilder revsvrUninstallCommands = new StringBuilder();
+
+            foreach (var f in FilterNames)
             {
-                if (f.EndsWith("x64.dll") && !Environment.Is64BitOperatingSystem)
+                if (f.Contains("x64") && !Environment.Is64BitOperatingSystem)
                     continue; // skip 64 assy on 32 bit systems
 
-                var file = ResourcesEx.WriteResourceToFile(f, _installDirectory);
+                var file = ResourcesEx.WriteResourceToFile(f, InstallDirectory);
                 var code = regsvr32(true, file);
                 if (code != 0)
                 {
                     Uninstall(assetPath);
                     throw new Exception("regsvr32 returned non-zero exit code: " + code);
                 }
+
+                revsvrUninstallCommands.AppendLine(regsvr32_command(false, file));
             }
+
+            //This works great, but it might be a good idea to move the cd command to the start (this insures that the path is also available to the elevated script -
+            // otherwise the elevated script just runs from system32). You should also redirect the net command to nul to hide it's output: net session >nul 2>&1
+
+            var unstallFileText = $@"
+NET SESSION
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
+GOTO ADMINTASKS
+
+:ELEVATE
+CD /d %~dp0
+MSHTA ""javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();""
+EXIT
+
+:ADMINTASKS
+{revsvrUninstallCommands.ToString()}
+
+reg delete ""HKEY_CURRENT_USER\{Constants.UninstallRegistryPath}\{Constants.DirectShowAppName}"" /f
+
+start /b """" cmd /c rd /s /q ""%~dp0"" & msg * /self /w ""Uninstallation of {Constants.DirectShowAppName} has been successful""";
+
+            var uninstallFilePath = Path.Combine(InstallDirectory, "uninstall.bat");
+
+            File.WriteAllText(uninstallFilePath, unstallFileText);
+            File.WriteAllText(Path.Combine(InstallDirectory, "readme.txt"), "Do not delete the files in this directory without first running the 'uninstall.bat' file." +
+                "\r\nIt will unregister the assemblies from COM and then this folder can be safely deleted.");
+
+            // write icon for uninstall programs list
+            var programIcon = ResourcesEx.WriteResourceToFile("default.ico", InstallDirectory);
+
+            var info = new ControlPanel.ControlPanelInfo()
+            {
+                DisplayName = Constants.DirectShowAppName,
+                UninstallString = uninstallFilePath,
+                InstallDirectory = InstallDirectory,
+                DisplayIconPath = programIcon,
+            };
+
+            ControlPanel.ControlPanelInfo.Install(Constants.DirectShowAppName, info);
         }
 
         public bool NeedsPrivileges()
@@ -56,22 +101,29 @@ namespace Clowd.Installer.Features
 
         public void Uninstall(string assetPath)
         {
+            if (!CheckInstalled(assetPath))
+                return;
+
             if (!SystemEx.IsProcessElevated)
             {
                 Program.Elevate(false, this.GetType(), assetPath);
                 return;
             }
 
-            if (Directory.Exists(_installDirectory))
-                foreach (var f in Directory.EnumerateFiles(_installDirectory))
+            string[] getBinaries() => Directory.GetFiles(InstallDirectory).Where(f => f.EndsWith("dll") || f.EndsWith("ax")).ToArray();
+
+            if (Directory.Exists(InstallDirectory))
+                foreach (var f in getBinaries())
                     if (0 == regsvr32(false, f))
                         File.Delete(f);
 
-            var files = Directory.GetFiles(_installDirectory);
+            var files = getBinaries();
             if (!files.Any())
-                Directory.Delete(_installDirectory);
+                Directory.Delete(InstallDirectory, true);
             else
                 throw new Exception("regsvr32 unable to uninstall files: \n" + String.Join("\n", files));
+
+            ControlPanel.ControlPanelInfo.Uninstall(Constants.DirectShowAppName);
         }
 
         private int regsvr32(bool install, string filepath)
@@ -80,6 +132,12 @@ namespace Clowd.Installer.Features
             var p = Process.Start("regsvr32", $"/s {uflag}\"{filepath}\"");
             p.WaitForExit();
             return p.ExitCode;
+        }
+
+        private string regsvr32_command(bool install, string filepath)
+        {
+            var uflag = install ? "" : "/u ";
+            return $"regsvr32 /s {uflag}\"{filepath}\"";
         }
     }
 }
