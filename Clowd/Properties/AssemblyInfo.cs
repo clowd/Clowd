1a1dae481732981c4e0525a52a1b4471e2a53532
Caelan Sayler <git@caesay.com>
2020-08-18 02:43:38 +0100

    WIP on new directshow video filter
    Former-commit-id: 8651cdc0ebdca29b55ff1b7038fd6596246d71fa

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.40.1620")]
-[assembly: AssemblyFileVersion("2.3.40.1620")]
+[assembly: AssemblyVersion("2.3.40.1623")]
+[assembly: AssemblyFileVersion("2.3.40.1623")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
