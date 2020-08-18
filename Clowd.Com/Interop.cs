604dbd892d576a875f7c5e91f1d9ded493f6f1bc
Caelan Sayler <git@caesay.com>
2020-08-18 08:51:38 +0100

    WIP Com filter. it works!
    Former-commit-id: fb947369030a23b493e099961206b92671db7912

--- Clowd.Com/Interop.cs
+++ Clowd.Com/Interop.cs
@@ -36,6 +36,13 @@ namespace Clowd.Com
         [DllImport("user32.dll")]
         public static extern bool DrawIcon(IntPtr hdc, int x, int y, IntPtr hIcon);
 
+
+        [DllImport("user32.dll")]
+        public static extern bool DrawIconEx(IntPtr hdc, int x, int y, IntPtr hIcon, int cx, int cy, uint istepIfAniCur, IntPtr hbrFlickerFreeDraw, DrawIconExFlags diFlags);
+
+        [DllImport("user32.dll")]
+        public static extern bool DestroyIcon(IntPtr hIcon);
+
         [DllImport("user32.dll")]
         public static extern bool GetIconInfo(IntPtr hIcon, out ICONINFO piconinfo);
 
@@ -100,6 +107,37 @@ namespace Clowd.Com
             Invalidate = 0x1,
             UpdateNow = 0x100,
         }
+
+        [Flags]
+        public enum DrawIconExFlags : uint
+        {
+            /// <summary>
+            /// Draws the icon or cursor using the mask.
+            /// </summary>
+            DI_MASK = 0x0001,
+            /// <summary>
+            /// Draws the icon or cursor using the image.
+            /// </summary>
+            DI_IMAGE = 0x0002,
+            /// <summary>
+            /// Combination of DI_IMAGE and DI_MASK.
+            /// </summary>
+            DI_NORMAL = 0x0003,
+            /// <summary>
+            /// This flag is ignored.
+            /// </summary>
+            DI_COMPAT = 0x0004,
+            /// <summary>
+            /// Draws the icon or cursor using the width and height specified by the system metric values for icons, 
+            /// if the cxWidth and cyWidth parameters are set to zero. If this flag is not specified and cxWidth and cyWidth are set to zero, 
+            /// the function uses the actual resource size.
+            /// </summary>
+            DI_DEFAULTSIZE = 0x0008,
+            /// <summary>
+            /// Draws the icon as an unmirrored icon. By default, the icon is drawn as a mirrored icon if hdc is mirrored.
+            /// </summary>
+            DI_NOMIRROR = 0x0010,
+        }
     }
     internal class GDI32
     {
@@ -108,7 +146,7 @@ namespace Clowd.Com
         public const int SRCINVERT = 0x00660046;
 
         [DllImport("gdi32.dll")]
-        public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hObjectSource, int nXSrc, int nYSrc, int dwRop);
+        public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hObjectSource, int nXSrc, int nYSrc, TernaryRasterOperations dwRop);
 
         [DllImport("gdi32.dll")]
         public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth, int nHeight);
@@ -137,5 +175,46 @@ namespace Clowd.Com
             public BitmapInfoHeader bmiHeader;
             public int[] bmiColors;
         }
+
+        [Flags]
+        public enum TernaryRasterOperations : uint
+        {
+            /// <summary>dest = source</summary>
+            SRCCOPY = 0x00CC0020,
+            /// <summary>dest = source OR dest</summary>
+            SRCPAINT = 0x00EE0086,
+            /// <summary>dest = source AND dest</summary>
+            SRCAND = 0x008800C6,
+            /// <summary>dest = source XOR dest</summary>
+            SRCINVERT = 0x00660046,
+            /// <summary>dest = source AND (NOT dest)</summary>
+            SRCERASE = 0x00440328,
+            /// <summary>dest = (NOT source)</summary>
+            NOTSRCCOPY = 0x00330008,
+            /// <summary>dest = (NOT src) AND (NOT dest)</summary>
+            NOTSRCERASE = 0x001100A6,
+            /// <summary>dest = (source AND pattern)</summary>
+            MERGECOPY = 0x00C000CA,
+            /// <summary>dest = (NOT source) OR dest</summary>
+            MERGEPAINT = 0x00BB0226,
+            /// <summary>dest = pattern</summary>
+            PATCOPY = 0x00F00021,
+            /// <summary>dest = DPSnoo</summary>
+            PATPAINT = 0x00FB0A09,
+            /// <summary>dest = pattern XOR dest</summary>
+            PATINVERT = 0x005A0049,
+            /// <summary>dest = (NOT dest)</summary>
+            DSTINVERT = 0x00550009,
+            /// <summary>dest = BLACK</summary>
+            BLACKNESS = 0x00000042,
+            /// <summary>dest = WHITE</summary>
+            WHITENESS = 0x00FF0062,
+            /// <summary>
+            /// Capture window as seen on screen.  This includes layered windows
+            /// such as WPF windows with AllowsTransparency="true"
+            /// </summary>
+            CAPTUREBLT = 0x40000000
+        }
+
     }
 }
