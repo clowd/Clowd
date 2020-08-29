bda202bf7b4257ebaaa2215951c10e9adb5b81c5
Caelan Sayler <git@caesay.com>
2020-08-29 18:00:07 +0100

    New GDI capturing
    Former-commit-id: 07c1f5bd56760e19ed97c1751492b870121e7654

--- Clowd.Interop/User32/USER32.cs
+++ Clowd.Interop/User32/USER32.cs
@@ -79,9 +79,11 @@ namespace Clowd.Interop
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool DestroyIcon(IntPtr hIcon);
 
-
         [DllImport("user32.dll")]
         public static extern bool DrawIcon(IntPtr hdc, int x, int y, IntPtr hIcon);
+
+        [DllImport("user32.dll")]
+        public static extern bool DrawIconEx(IntPtr hdc, int x, int y, IntPtr hIcon, int cx, int cy, uint istepIfAniCur, IntPtr hbrFlickerFreeDraw, DrawIconExFlags diFlags);
 
         [DllImport("user32.dll")]
         public static extern bool GetIconInfo(IntPtr hIcon, out ICONINFO piconinfo);
@@ -498,6 +500,37 @@ namespace Clowd.Interop
         South = 6,
         SouthWest = 7,
         SouthEast = 8
+    }
+
+    [Flags]
+    public enum DrawIconExFlags : uint
+    {
+        /// <summary>
+        /// Draws the icon or cursor using the mask.
+        /// </summary>
+        DI_MASK = 0x0001,
+        /// <summary>
+        /// Draws the icon or cursor using the image.
+        /// </summary>
+        DI_IMAGE = 0x0002,
+        /// <summary>
+        /// Combination of DI_IMAGE and DI_MASK.
+        /// </summary>
+        DI_NORMAL = 0x0003,
+        /// <summary>
+        /// This flag is ignored.
+        /// </summary>
+        DI_COMPAT = 0x0004,
+        /// <summary>
+        /// Draws the icon or cursor using the width and height specified by the system metric values for icons, 
+        /// if the cxWidth and cyWidth parameters are set to zero. If this flag is not specified and cxWidth and cyWidth are set to zero, 
+        /// the function uses the actual resource size.
+        /// </summary>
+        DI_DEFAULTSIZE = 0x0008,
+        /// <summary>
+        /// Draws the icon as an unmirrored icon. By default, the icon is drawn as a mirrored icon if hdc is mirrored.
+        /// </summary>
+        DI_NOMIRROR = 0x0010,
     }
 
     [Flags]
