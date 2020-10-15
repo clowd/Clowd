12ca0dc9579fc49d585e5e4ac4a87f7096148f32
Caelan Sayler <git@caesay.com>
2020-10-15 12:12:50 +0100

    Bump version to 2.3.40.1914

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.40.1848")]
-[assembly: AssemblyFileVersion("2.3.40.1848")]
+[assembly: AssemblyVersion("2.3.40.1914")]
+[assembly: AssemblyFileVersion("2.3.40.1914")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
