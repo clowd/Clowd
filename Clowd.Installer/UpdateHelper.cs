c5b608dfa1d5318a75213ab0797ed86644298599
Caelan Sayler <git@caesay.com>
2020-10-17 13:37:26 +0100

    Minor changes to NAU, add update helper to installer
    Former-commit-id: 289ed07813f77f86c4b75fb8956b051cffb48862

---
+++ Clowd.Installer/UpdateHelper.cs
@@ -0,0 +1,135 @@
+﻿using NAppUpdate.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Clowd.Installer
+{
+    public static class UpdateHelper
+    {
+        public static UpdateManager GetUpdaterInstance()
+        {
+            // NAppUpdater uses relative paths, so the current directory must be set accordingly.
+            Environment.CurrentDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var _updateManager = UpdateManager.Instance;
+            _updateManager.Config.UpdateExecutableName = "clowd-upd.exe";
+            _updateManager.Config.TempFolder = Path.Combine(Constants.AppDataDirectory, "update");
+            _updateManager.Config.BackupFolder = Path.Combine(Constants.AppDataDirectory, "backup");
+            _updateManager.Config.UpdateProcessName = "ClowdUpdate";
+
+            return _updateManager;
+        }
+
+        public static async Task<UpdatePackage> GetLatestChannelReleaseAsync(string channel = null)
+        {
+            var avl = await GetAvailablePackagesAsync();
+
+            if (String.IsNullOrEmpty(channel))
+                channel = avl.MainChannel;
+
+            var rel = avl.Packages
+                .Where(p => p.Channel.Equals(channel, StringComparison.OrdinalIgnoreCase))
+                .OrderByDescending(p => p.Version)
+                .FirstOrDefault();
+
+            return rel;
+        }
+
+        public static UpdatePackage GetLatestChannelRelease(string channel = null)
+        {
+            var avl = GetAvailablePackages();
+
+            if (String.IsNullOrEmpty(channel))
+                channel = avl.MainChannel;
+
+            var rel = avl.Packages
+                .Where(p => p.Channel.Equals(channel, StringComparison.OrdinalIgnoreCase))
+                .OrderByDescending(p => p.Version)
+                .FirstOrDefault();
+
+            return rel;
+        }
+
+        public static async Task<AvailablePackagesResult> GetAvailablePackagesAsync()
+        {
+            using (var wc = new WebClient())
+            {
+                var feed = await wc.DownloadStringTaskAsync(Constants.ReleaseFeedUrl);
+                var doc = XDocument.Parse(feed);
+
+                var defaultChannel = doc.Root.Attribute("MainChannel").Value;
+                var packages = new List<UpdatePackage>();
+
+                foreach (var el in doc.Root.Elements("Package"))
+                {
+                    var version = el.Attribute("Version").Value;
+                    var channel = el.Attribute("Channel").Value;
+                    var url = el.Attribute("FeedUrl").Value;
+
+                    packages.Add(new UpdatePackage()
+                    {
+                        Version = version,
+                        Channel = channel,
+                        FeedUrl = url
+                    });
+                }
+
+                return new AvailablePackagesResult()
+                {
+                    MainChannel = defaultChannel,
+                    Packages = packages,
+                };
+            }
+        }
+
+        public static AvailablePackagesResult GetAvailablePackages()
+        {
+            using (var wc = new WebClient())
+            {
+                var feed = wc.DownloadString(Constants.ReleaseFeedUrl);
+                var doc = XDocument.Parse(feed);
+
+                var defaultChannel = doc.Root.Attribute("MainChannel").Value;
+                var packages = new List<UpdatePackage>();
+
+                foreach (var el in doc.Root.Elements("Package"))
+                {
+                    var version = el.Attribute("Version").Value;
+                    var channel = el.Attribute("Channel").Value;
+                    var url = el.Attribute("FeedUrl").Value;
+
+                    packages.Add(new UpdatePackage()
+                    {
+                        Version = version,
+                        Channel = channel,
+                        FeedUrl = url
+                    });
+                }
+
+                return new AvailablePackagesResult()
+                {
+                    MainChannel = defaultChannel,
+                    Packages = packages,
+                };
+            }
+        }
+    }
+
+    public class AvailablePackagesResult
+    {
+        public string MainChannel { get; set; }
+        public List<UpdatePackage> Packages { get; set; }
+    }
+
+    public class UpdatePackage
+    {
+        public string Version { get; set; }
+        public string Channel { get; set; }
+        public string FeedUrl { get; set; }
+    }
+}
