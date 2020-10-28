db2128c1ddb4612daa0f02dbf5d79cb08e713f44
Caelan <git@caesay.com>
2020-10-28 22:30:53 +0000

    Add settings UI for new Upload library
    Former-commit-id: f5074c6eb2af4f112171c9baee7747b33c44d85a

---
+++ Clowd/Converters/EnumMatchToBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Clowd.Converters
+{
+    [ValueConversion(typeof(Enum), typeof(bool))]
+    public class EnumMatchToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var matchVal = (Enum)Enum.Parse(value.GetType(), parameter as string, true);
+            return (value as Enum)?.HasFlag(matchVal) ?? false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Clowd/SettingsPage.xaml.cs
+++ Clowd/SettingsPage.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using Clowd.Controls;
+using Clowd.Installer.Features;
 using Clowd.Utilities;
 using PropertyChanged;
 using PropertyTools.DataAnnotations;
@@ -117,68 +118,196 @@ namespace Clowd
     {
         public override FrameworkElement CreateControl(PropertyItem property, PropertyControlFactoryOptions options)
         {
-            if (property.Is(typeof(GlobalTrigger)))
+            FrameworkElement SimpleControlBinding(FrameworkElement control, DependencyProperty dependency)
             {
-                var editor = new GlobalTriggerEditor();
-                editor.SetBinding(GlobalTriggerEditor.TriggerProperty, property.CreateBinding());
-                return editor;
+                var bind = property.CreateBinding();
+                bind.Source = property.TargetObject;
+                control.SetBinding(dependency, bind);
+                return control;
             }
 
-            //if (typeof(FFMpegCodecSettings).IsAssignableFrom(property.ActualPropertyType))
-            //{
-            //    var editor = new FFMpegCodecSettingsEditor(() => { App.Current.Settings.SaveQuiet(); });
-            //    editor.SetBinding(FFMpegCodecSettingsEditor.CodecSettingsProperty, property.CreateBinding());
-            //    return editor;
-            //}
+            FrameworkElement ComboSelectBinding(Func<System.Collections.IEnumerable> items, string displayPath, bool canClear = true)
+            {
+                var bind = property.CreateBinding();
+                bind.Source = property.TargetObject;
+
+                var panel = new DockPanel();
+                var reset = new Button();
+                var combo = new ComboBox();
+
+                if (canClear)
+                {
+                    DockPanel.SetDock(reset, Dock.Right);
+                    reset.Margin = new Thickness(5, 0, 0, 0);
+                    reset.Padding = new Thickness(10, 0, 10, 0);
+                    reset.Content = "Clear";
+                    reset.Click += (s, e) => { combo.SelectedIndex = -1; };
+                    panel.Children.Add(reset);
+                }
+
+                combo.DisplayMemberPath = displayPath;
+                combo.ItemsSource = items();
+                combo.DropDownOpened += (s, e) => { combo.ItemsSource = items(); };
+                combo.SelectionChanged += (s, e) => { App.Current?.Settings?.SaveQuiet(); };
+                combo.SetBinding(ComboBox.SelectedItemProperty, bind);
+                panel.Children.Add(combo);
+
+                return panel;
+            }
+
+            FrameworkElement ButtonControl(string buttonText, RoutedEventHandler buttonClick, FrameworkElement top = null, FrameworkElement right = null, FrameworkElement bottom = null)
+            {
+                StackPanel panel = new StackPanel();
+                panel.Orientation = Orientation.Vertical;
+
+                var btn = new Button();
+                btn.Padding = new Thickness(10, 0, 10, 0);
+                btn.Margin = new Thickness(0, top == null ? 0 : 5, right == null ? 0 : 5, bottom == null ? 0 : 5);
+                btn.Content = buttonText;
+                btn.Click += buttonClick;
+
+                if (top != null)
+                    panel.Children.Add(top);
+
+                if (right == null)
+                {
+                    panel.Children.Add(btn);
+                }
+                else
+                {
+                    StackPanel panel2 = new StackPanel();
+                    panel2.Orientation = Orientation.Horizontal;
+                    panel2.Children.Add(btn);
+                    panel2.Children.Add(right);
+                    panel.Children.Add(panel2);
+                }
+
+                if (bottom != null)
+                    panel.Children.Add(bottom);
+
+                return panel;
+            }
+
+            FrameworkElement PageBinding(FrameworkElement control, DependencyProperty dependency, string windowText, string buttonText, FrameworkElement top = null, FrameworkElement right = null, FrameworkElement bottom = null)
+            {
+                var page = SimpleControlBinding(control, dependency);
+                var width = page.Width;
+                var height = page.Height;
+                var click = new RoutedEventHandler((s, e) =>
+                {
+                    var window = TemplatedWindow.GetWindow(page);
+                    if (window != null)
+                    {
+                        var windowTarget = page.GetValue(dependency);
+                        if (windowTarget == GetPropertyValue<object>())
+                        {
+                            window.MakeForeground();
+                            return;
+                        }
+                    }
+
+                    window = TemplatedWindow.CreateWindow(windowText, page);
+                    window.Width = width;
+                    window.Height = height;
+                    window.Show();
+                });
+
+                return ButtonControl(buttonText, click, top, right, bottom);
+            }
+
+            T GetPropertyValue<T>() where T : class
+            {
+                var pinfo = property.GetDescriptor(property.PropertyName);
+                var val = pinfo.GetValue(property.TargetObject) as T;
+                return val;
+            }
+
+            if (property.Is(typeof(GlobalTrigger)))
+            {
+                return SimpleControlBinding(new GlobalTriggerEditor(), GlobalTriggerEditor.TriggerProperty);
+            }
 
             if (property.Is(typeof(FFmpegSettings)))
             {
-                var editor2 = new FFMpegCodecSettingsEditor3();
-                var bind = property.CreateBinding();
-                bind.Source = property.TargetObject;
-                editor2.SetBinding(FFMpegCodecSettingsEditor3.CodecSettingsProperty, bind);
-                return editor2;
+                return SimpleControlBinding(new FFMpegCodecSettingsEditor3(), FFMpegCodecSettingsEditor3.CodecSettingsProperty);
             }
 
             if (property.Is(typeof(FFmpegDirectShowAudioDevice)))
             {
-                var bind = property.CreateBinding();
-                bind.Source = property.TargetObject;
-                var combo = new ComboBox();
-                combo.DisplayMemberPath = nameof(FFmpegDirectShowAudioDevice.FriendlyName);
-                combo.ItemsSource = FFmpegDirectShowAudioDevice.GetDevices();
-                combo.SetBinding(ComboBox.SelectedItemProperty, bind);
-                return combo;
+                return ComboSelectBinding(() => FFmpegDirectShowAudioDevice.GetDevices(), nameof(FFmpegDirectShowAudioDevice.FriendlyName));
             }
 
-            if (typeof(Installer.Features.IFeature).IsAssignableFrom(property.ActualPropertyType))
+            if (property.Is(typeof(IFeature)))
             {
-                var pinfo = property.GetDescriptor(property.PropertyName);
-                var val = pinfo.GetValue(property.TargetObject) as Installer.Features.IFeature;
-                return new FeatureInstallerControl(val);
+                return new FeatureInstallerControl(GetPropertyValue<IFeature>());
             }
 
-            if (typeof(FrameworkElement).IsAssignableFrom(property.ActualPropertyType))
+            if (property.Is(typeof(IUploadProvider)))
             {
-                var pinfo = property.GetDescriptor(property.PropertyName);
-                var val = pinfo.GetValue(property.TargetObject) as FrameworkElement;
+                var propertyName = property.Descriptor.Name;
+                var parsedEnum = (SupportedUploadType)Enum.Parse(typeof(SupportedUploadType), propertyName, true);
+
+                IEnumerable<IUploadProvider> getUploadProviders()
+                {
+                    return App.Current.Settings.UploadSettings.Providers
+                        .Where(p => p.IsEnabled)
+                        .Where(p => p.SupportedUpload == SupportedUploadType.All || p.SupportedUpload.HasFlag(parsedEnum));
+                }
+
+                return ComboSelectBinding(getUploadProviders, nameof(IUploadProvider.Name));
+            }
+
+            if (property.Is(typeof(List<IUploadProvider>)))
+            {
+                var val = GetPropertyValue<List<IUploadProvider>>();
+
+                var totalCount = val.Count();
+                var enabledCount = val.Where(p => p.IsEnabled).Count();
+
+                return PageBinding(
+                    new UploadProviderSettingsEditor(),
+                    UploadProviderSettingsEditor.ProvidersProperty,
+                    "Configure Upload Providers",
+                    "Configure",
+                    new TextBlock()
+                    {
+                        TextWrapping = TextWrapping.Wrap,
+                        Margin = new Thickness(0, 5, 0, 0),
+                        Text = $"{enabledCount}/{totalCount} available providers are enabled."
+                    },
+                    null,
+                    new TextBlock()
+                    {
+                        TextWrapping = TextWrapping.Wrap,
+                        Foreground = Brushes.Gray,
+                        Margin = new Thickness(0, 0, 0, 10),
+                        Text = $"First, configure upload providers and then select which provider you would like to be triggered for different file types. " +
+                        $"Not all providers support all file types (ex. YouTube, Imgur). " +
+                        $"Leaving the provider for a specific file type blank will ask you every time." +
+                        Environment.NewLine +
+                        $"0 providers are currently loaded by user-added modules."
+                    }
+                );
+            }
+
+            if (property.Is(typeof(FrameworkElement)))
+            {
+                var val = GetPropertyValue<FrameworkElement>();
+                // if the control was used elsewhere prior, we need to reset the logical tree before using it again
                 val.DisconnectFromLogicalParent();
                 return val;
             }
 
-            if (property.ActualPropertyType.IsGenericType && property.ActualPropertyType.GetGenericTypeDefinition() == typeof(AutoDictionary<,>))
+            if (property.Is(typeof(AutoDictionary<,>)))
             {
-                var btn = new Button();
-                btn.Content = "Reset";
-                btn.Click += async (s, e) =>
+                return ButtonControl("Reset", async (s, e) =>
                 {
-                    if (await NiceDialog.ShowYesNoPromptAsync(btn, NiceDialogIcon.Warning, "Are you sure you wish to reset these settings to defaults?"))
+                    if (await NiceDialog.ShowYesNoPromptAsync(s as FrameworkElement, NiceDialogIcon.Warning, "Are you sure you wish to reset these settings to defaults?"))
                     {
                         var pinfo = property.GetDescriptor(property.PropertyName);
                         pinfo.SetValue(property.TargetObject, Activator.CreateInstance(property.ActualPropertyType));
                     }
-                };
-                return btn;
+                });
             }
 
             return base.CreateControl(property, options);
