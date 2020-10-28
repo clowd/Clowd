d5aa40114e5ef13f851933b953e08f15bba8729d
Caelan Sayler <git@caesay.com>
2020-10-28 22:28:56 +0000

    Add new Upload lib (replaces FileUploadLib)
    Former-commit-id: fff215ac427db4a8684fa916328e819246e9d648

---
+++ Clowd.Upload/MimeDbMimeProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Clowd.Upload
+{
+    public class MimeDbMimeProvider : IMimeProvider
+    {
+        // the following url is locked to a specific commit, so this should be updated periodically.
+        private const string mimePath = "https://raw.githubusercontent.com/jshttp/mime-db/63731b4b161970d3205dda268102a44ea9436254/db.json";
+
+        private static Dictionary<string, MimeDbMimeEntry> _database;
+        private static DateTime _databaseRefreshTime;
+
+        private static async Task EnsureMimeCache()
+        {
+            if (_database != null)
+                return;
+
+            using (WebClient wc = new WebClient())
+            {
+                var txt = await wc.DownloadStringTaskAsync(mimePath);
+                var obj = JsonConvert.DeserializeObject<Dictionary<string, MimeDbMimeEntry>>(txt);
+                foreach (var kvp in obj)
+                {
+                    kvp.Value.ContentType = kvp.Key;
+                    kvp.Value.Extensions = kvp.Value.Extensions ?? new string[0];
+                }
+
+                _database = obj;
+                _databaseRefreshTime = DateTime.Now;
+            }
+        }
+
+        public async Task<IMimeEntry> GetMimeFromExtension(string extension)
+        {
+            await EnsureMimeCache();
+            extension = extension.ToLower().Trim('.');
+            return _database.Values.FirstOrDefault(o => o.Extensions.Contains(extension)) ?? await GetDefaultDownloadMime();
+        }
+
+        public async Task<IMimeEntry> GetDefaultDownloadMime()
+        {
+            await EnsureMimeCache();
+            return _database["application/octet-stream"];
+        }
+
+        private class MimeDbMimeEntry : IMimeEntry
+        {
+            public string ContentType { get; set; }
+            public string Source { get; set; }
+            public string[] Extensions { get; set; }
+            public bool? Compressible { get; set; }
+            public string Charset { get; set; }
+        }
+    }
+
+    public interface IMimeProvider
+    {
+        Task<IMimeEntry> GetMimeFromExtension(string extension);
+        Task<IMimeEntry> GetDefaultDownloadMime();
+    }
+
+    public interface IMimeEntry
+    {
+        string ContentType { get; }
+        string Source { get; }
+        string[] Extensions { get; }
+        bool? Compressible { get; }
+        string Charset { get; }
+    }
+}
