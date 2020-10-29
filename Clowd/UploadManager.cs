ee495f25e59c9dc48e84a6fcd6c664c663fbc415
Caelan <git@caesay.com>
2020-10-29 17:37:33 +0000

    Clean up solution nuget packages
    Former-commit-id: 3721aa677d60d045a0ec9f3d253163dd499f3572

--- Clowd/UploadManager.cs
+++ Clowd/UploadManager.cs
@@ -7,7 +7,6 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
 using RT.Util.ExtensionMethods;
 using Clowd.Shared;
 using System.Windows;
