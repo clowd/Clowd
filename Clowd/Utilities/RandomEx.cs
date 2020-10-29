6b808327fa5c050e97d7bd6be1b0a266ccb83933
Caelan <git@caesay.com>
2020-10-29 18:18:30 +0000

    Remove CS.Util / CS.WPF; Rename color picker dll
    Former-commit-id: 00fc01f6c75bf64b1203e9f78d368b580d9a789a

---
+++ Clowd/Utilities/RandomEx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Util
+{
+    public static class RandomEx
+    {
+        private static readonly Random _random = new Random();
+        private static readonly object _lock = new object();
+        private static readonly Guid _namespace = new Guid("527e4824-8fa0-4f47-aa27-5259fcb54b23");
+
+        public static int GetInt32()
+        {
+            lock (_lock)
+            {
+                return _random.Next();
+            }
+        }
+        public static int GetInt32(int max)
+        {
+            lock (_lock)
+            {
+                return _random.Next(max);
+            }
+        }
+        public static int GetInt32(int min, int max)
+        {
+            lock (_lock)
+            {
+                return _random.Next(min, max);
+            }
+        }
+
+        public static long GetInt64()
+        {
+            return GetInt64(0, long.MaxValue);
+        }
+        public static long GetInt64(long max)
+        {
+            return GetInt64(0, max);
+        }
+        public static long GetInt64(long min, long max)
+        {
+            //http://stackoverflow.com/a/13095144/184746
+            if (max <= min)
+                throw new ArgumentOutOfRangeException("max", "max must be > min!");
+
+            //Working with ulong so that modulo works correctly with values > long.MaxValue
+            ulong uRange = (ulong)(max - min);
+
+            //Prevent a modolo bias; see http://stackoverflow.com/a/10984975/238419
+            lock (_lock)
+            {
+                ulong ulongRand;
+                do
+                {
+                    byte[] buf = new byte[8];
+                    _random.NextBytes(buf);
+                    ulongRand = (ulong)BitConverter.ToInt64(buf, 0);
+                } while (ulongRand > ulong.MaxValue - ((ulong.MaxValue % uRange) + 1) % uRange);
+                return (long)(ulongRand % uRange) + min;
+            }
+        }
+
+        public static Guid GetGuid()
+        {
+            return Guid.NewGuid();
+        }
+        public static Guid GetGuid(string name)
+        {
+            return GetGuid(name, _namespace, 5);
+        }
+        public static Guid GetGuid(string name, Guid gNamespace, int version)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            if (version != 3 && version != 5)
+                throw new ArgumentOutOfRangeException("version", "version must be either 3 or 5.");
+
+
+            // convert the name to a sequence of octets (as defined by the standard or conventions of its namespace) (step 3)
+            // ASSUME: UTF-8 encoding is always appropriate
+            byte[] nameBytes = Encoding.UTF8.GetBytes(name);
+
+            // convert the namespace UUID to network order (step 3)
+            byte[] namespaceBytes = gNamespace.ToByteArray();
+            SwapByteOrder(namespaceBytes);
+
+            // compute the hash of the name space ID concatenated with the name (step 4)
+            byte[] hash;
+            using (HashAlgorithm algorithm = version == 3 ? (HashAlgorithm)MD5.Create() : SHA1.Create())
+            {
+                algorithm.TransformBlock(namespaceBytes, 0, namespaceBytes.Length, null, 0);
+                algorithm.TransformFinalBlock(nameBytes, 0, nameBytes.Length);
+                hash = algorithm.Hash;
+            }
+
+            // most bytes from the hash are copied straight to the bytes of the new GUID (steps 5-7, 9, 11-12)
+            byte[] newGuid = new byte[16];
+            Array.Copy(hash, 0, newGuid, 0, 16);
+
+            // set the four most significant bits (bits 12 through 15) of the time_hi_and_version field to the appropriate 4-bit version number from Section 4.1.3 (step 8)
+            newGuid[6] = (byte)((newGuid[6] & 0x0F) | (version << 4));
+
+            // set the two most significant bits (bits 6 and 7) of the clock_seq_hi_and_reserved to zero and one, respectively (step 10)
+            newGuid[8] = (byte)((newGuid[8] & 0x3F) | 0x80);
+
+            // convert the resulting UUID to local byte order (step 13)
+            SwapByteOrder(newGuid);
+            return new Guid(newGuid);
+        }
+
+        public static string GetString(int length)
+        {
+            lock (_lock)
+            {
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                return new string(Enumerable.Repeat(chars, length)
+                    .Select(s => s[_random.Next(s.Length)]).ToArray());
+            }
+        }
+        public static string GetCryptoUniqueString(int length)
+        {
+            const string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            if (length < 0) throw new ArgumentOutOfRangeException("length", "length cannot be less than zero.");
+            //if (string.IsNullOrEmpty(allowedChars)) throw new ArgumentException("allowedChars may not be empty.");
+
+            const int byteSize = 0x100;
+            var allowedCharSet = new HashSet<char>(allowedChars).ToArray();
+            if (byteSize < allowedCharSet.Length) throw new ArgumentException(String.Format("allowedChars may contain no more than {0} characters.", byteSize));
+
+            // Guid.NewGuid and System.Random are not particularly random. By using a
+            // cryptographically-secure random number generator, the caller is always
+            // protected, regardless of use.
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var result = new StringBuilder();
+                var buf = new byte[128];
+                while (result.Length < length)
+                {
+                    rng.GetBytes(buf);
+                    for (var i = 0; i < buf.Length && result.Length < length; ++i)
+                    {
+                        // Divide the byte into allowedCharSet-sized groups. If the
+                        // random value falls into the last group and the last group is
+                        // too small to choose from the entire allowedCharSet, ignore
+                        // the value in order to avoid biasing the result.
+                        var outOfRangeStart = byteSize - (byteSize % allowedCharSet.Length);
+                        if (outOfRangeStart <= buf[i]) continue;
+                        result.Append(allowedCharSet[buf[i] % allowedCharSet.Length]);
+                    }
+                }
+                return result.ToString();
+            }
+        }
+
+        // Converts a GUID (expressed as a byte array) to/from network order (MSB-first).
+        private static void SwapByteOrder(byte[] guid)
+        {
+            SwapBytes(guid, 0, 3);
+            SwapBytes(guid, 1, 2);
+            SwapBytes(guid, 4, 5);
+            SwapBytes(guid, 6, 7);
+        }
+        private static void SwapBytes(byte[] guid, int left, int right)
+        {
+            byte temp = guid[left];
+            guid[left] = guid[right];
+            guid[right] = temp;
+        }
+    }
+}
