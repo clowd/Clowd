e9d81b39b5ec2f5635a102c014d4abc65f0d3291
Caelan <git@caesay.com>
2020-11-03 11:38:55 +0000

    Bump version to 2.4.42.1948
    Former-commit-id: bdd90e5501509702fcad5ed6372a4eaf24afc960

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.3.42.1938")]
-[assembly: AssemblyFileVersion("2.3.42.1938")]
+[assembly: AssemblyVersion("2.4.42.1948")]
+[assembly: AssemblyFileVersion("2.4.42.1948")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
