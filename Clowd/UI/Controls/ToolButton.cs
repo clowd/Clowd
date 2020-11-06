4a0fcf023592fe9d9bf398ab5579869fe6fc270b
Caelan Sayler <git@caesay.com>
2020-11-06 12:19:39 +0000

    Clean up ImageEditorPage.xaml; create Toolbutton
    Former-commit-id: 1e3386494986c3b1dea25e0ae6c9b93752dd6dad

---
+++ Clowd/UI/Controls/ToolButton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Clowd.UI.Controls
+{
+    class ToolButton : ToggleButton
+    {
+        public UIElement IconPath
+        {
+            get { return (UIElement)GetValue(IconPathProperty); }
+            set { SetValue(IconPathProperty, value); }
+        }
+
+        public static readonly UIElement IconPathDefaultValue = null;
+
+        public static readonly DependencyProperty IconPathProperty =
+            DependencyProperty.Register(nameof(IconPath), typeof(UIElement), typeof(ToolButton),
+                new PropertyMetadata(IconPathDefaultValue, (s, e) => (s as ToolButton)?.OnIconPathChanged(s, e)));
+
+        public event DependencyPropertyChangedEventHandler IconPathChanged;
+
+        protected virtual void OnIconPathChanged(object sender, DependencyPropertyChangedEventArgs e)
+            => this.IconPathChanged?.Invoke(sender, e);
+
+        public bool CanToggle
+        {
+            get { return (bool)GetValue(CanToggleProperty); }
+            set { SetValue(CanToggleProperty, value); }
+        }
+
+        public static readonly bool CanToggleDefaultValue = false;
+
+        public static readonly DependencyProperty CanToggleProperty =
+            DependencyProperty.Register(nameof(CanToggle), typeof(bool), typeof(ToolButton),
+                new PropertyMetadata(CanToggleDefaultValue, (s, e) => (s as ToolButton)?.OnCanToggleChanged(s, e)));
+
+        public event DependencyPropertyChangedEventHandler CanToggleChanged;
+
+        protected virtual void OnCanToggleChanged(object sender, DependencyPropertyChangedEventArgs e)
+            => this.CanToggleChanged?.Invoke(sender, e);
+
+        static ToolButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToolButton), new FrameworkPropertyMetadata(typeof(ToolButton)));
+        }
+
+        public ToolButton()
+        {
+        }
+
+        protected override void OnToggle()
+        {
+            if (CanToggle)
+                base.OnToggle();
+        }
+    }
+}
