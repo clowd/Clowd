92812fb5315a941e9ea47c883c9e28d11f2b92d3
Caelan Sayler <git@caesay.com>
2021-02-10 14:04:17 +0000

    Remove x86 version of ffmpeg
    Former-commit-id: 600ef61f5298ca1d37141ab620d1fb971a455968

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.4.47.1974")]
-[assembly: AssemblyFileVersion("2.4.47.1974")]
+[assembly: AssemblyVersion("2.4.47.1975")]
+[assembly: AssemblyFileVersion("2.4.47.1975")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
