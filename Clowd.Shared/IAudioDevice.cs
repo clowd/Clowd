80786f211f985256d48b64e585d504abbbd54855
Caelan <git@caesay.com>
2021-02-10 15:46:33 +0000

    Move audio handling to Clowd.Video
    Former-commit-id: 60808483456c361a65a3fe335a20be11dc620c46

--- Clowd.Shared/IAudioDevice.cs
+++ Clowd.Shared/IAudioDevice.cs
@@ -10,8 +10,14 @@ namespace Clowd
     {
         string DeviceId { get; }
         string FriendlyName { get; }
+        IAudioLevelListener GetLevelListener();
     }
 
     public interface IAudioSpeakerDevice : IAudioDevice { }
     public interface IAudioMicrophoneDevice : IAudioDevice { }
+
+    public interface IAudioLevelListener : IDisposable
+    {
+        double GetPeakLevel();
+    }
 }
---
+++ Clowd.Video/AudioDeviceManager.cs
@@ -0,0 +1,109 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowd.Video
+{
+    public class AudioDeviceManager
+    {
+        public IEnumerable<IAudioSpeakerDevice> GetSpeakers()
+        {
+            return GetDevices(DataFlow.Render).Select(m => new NAudioSpeakerDevice(m));
+        }
+
+        public IEnumerable<IAudioMicrophoneDevice> GetMicrophones()
+        {
+            return GetDevices(DataFlow.Capture).Select(m => new NAudioMicrophoneDevice(m));
+        }
+
+        protected static IEnumerable<MMDevice> GetDevices(DataFlow flow)
+        {
+            using (MMDeviceEnumerator enumerator = new MMDeviceEnumerator())
+            {
+                foreach (MMDevice device in enumerator.EnumerateAudioEndPoints(flow, DeviceState.Active))
+                {
+                    yield return device;
+                }
+            }
+        }
+    }
+
+    internal class NAudioMicrophoneDevice : NAudioDevice, IAudioMicrophoneDevice
+    {
+        public NAudioMicrophoneDevice(MMDevice device) : base(device)
+        {
+        }
+    }
+
+    internal class NAudioSpeakerDevice : NAudioDevice, IAudioSpeakerDevice
+    {
+        public NAudioSpeakerDevice(MMDevice device) : base(device)
+        {
+        }
+    }
+
+    internal class NAudioDevice : IAudioDevice
+    {
+        public string DeviceId => _device.ID;
+
+        public string FriendlyName => _device.FriendlyName;
+
+        private MMDevice _device;
+
+        public NAudioDevice(MMDevice device)
+        {
+            _device = device;
+        }
+
+        public IAudioLevelListener GetLevelListener()
+        {
+            return new LevelListener(_device);
+        }
+    }
+
+    internal class LevelListener : IAudioLevelListener
+    {
+        private readonly MMDevice _device;
+        private readonly AudioClient _audioClient;
+        private bool _disposed = false;
+
+        ~LevelListener()
+        {
+            Dispose();
+        }
+
+        public LevelListener(MMDevice device)
+        {
+            _audioClient = device.AudioClient;
+            _audioClient.Initialize(AudioClientShareMode.Shared,
+               AudioClientStreamFlags.None,
+               100,
+               100,
+               _audioClient.MixFormat,
+               Guid.Empty);
+            _audioClient.Start();
+            this._device = device;
+        }
+
+        public double GetPeakLevel()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(LevelListener));
+            return _device.AudioMeterInformation.MasterPeakValue;
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _audioClient.Stop();
+                _audioClient.Dispose();
+                _disposed = true;
+            }
+            GC.SuppressFinalize(this);
+        }
+    }
+}
