86f526833682c9329beb2aaa2758494a89d9506b
Caelan <git@caesay.com>
2021-02-10 15:47:21 +0000

    Propagate FFmpeg logs through IVideoCapturer interface
    Former-commit-id: 5c9ee55a8041c5c2078999e00ed05ef28a1742ed

--- Clowd.Video/FFmpegCapturer.cs
+++ Clowd.Video/FFmpegCapturer.cs
@@ -13,10 +13,12 @@ namespace Clowd.Video
     {
         private LiveScreenRecording _recorder;
         private Task _process;
+
         public override Task<string> StartAsync(Rectangle captureRect, VideoCapturerSettings settings)
         {
             BusyStatus = "Starting...";
             _recorder = new LiveScreenRecording(captureRect, settings);
+            _recorder.LogReceived += Recording_LogRecieved;
             _process = _recorder.Start().ContinueWith(t =>
             {
                 if (t.Exception != null)
@@ -50,6 +52,41 @@ namespace Clowd.Video
                 }
                 catch { }
             }
+        }
+
+        private void Recording_LogRecieved(object sender, FFMpegLogEventArgs e)
+        {
+            //frame=  219 fps= 31 q=10.0 size=       0kB time=00:00:05.80 bitrate=   0.1kbits/s dup=5 drop=0 speed=0.82x
+
+            string getData(string label)
+            {
+                var msg = e.Data;
+                var start = msg.IndexOf(label);
+                if (start < 0)
+                    return null;
+                msg = msg.Substring(start + label.Length).TrimStart();
+                msg = msg.Substring(0, msg.IndexOf(" "));
+                if (msg == "0.0" || msg == "00:00:00.00")
+                    return null;
+
+                return msg;
+            }
+
+            var fps = getData("fps=");
+            var time = getData("time=");
+            TimeSpan ts = default(TimeSpan);
+
+            if (time != null)
+            {
+                try
+                {
+                    // sometimes ffmpeg gives us garbage timecodes, it depends on the input stream timestamp on the frames & the settings we use.
+                    ts = TimeSpan.Parse(time);
+                }
+                catch { }
+            }
+
+            OnStatusRecieved((int)Convert.ToDouble(fps), 0, ts);
         }
     }
 }
