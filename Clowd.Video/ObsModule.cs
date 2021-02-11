e9efc32c6e9c2826c788cff9fe2c3791a7244678
Caelan Sayler <git@caesay.com>
2021-02-11 16:28:39 +0000

    WIP github module

--- Clowd.Video/ObsModule.cs
+++ Clowd.Video/ObsModule.cs
@@ -6,37 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Clowd
+namespace Clowd.Video
 {
-    public class Version
-    {
-        public string Ver { get; set; }
-        public bool IsPrerelease { get; set; }
-    }
-
-    public interface IModule
-    {
-    }
-
-    public interface IModuleInfo : INotifyPropertyChanged
-    {
-        string Name { get; }
-        string Description { get; }
-        Stream Icon { get; }
-        string InstalledVersion { get; }
-        string UpdateAvailable { get; }
-        bool Prerelease { get; }
-    }
-
-    public interface IModuleInfo<T> : IModuleInfo where T : IModule
-    {
-        Task CheckForUpdates(bool includePrereleases);
-        Task Install(string version);
-        Task Uninstall();
-        T GetNewInstance();
-    }
-
-    public abstract class GithubReleaseModule<T> : IModuleInfo<T> where T : IModule
+    public abstract class GithubReleaseModule<T> : IModuleInfo<T>
     {
         private string installedVersion;
         private string updateAvailable;
@@ -90,21 +62,12 @@ namespace Clowd
         public abstract string Name { get; }
         public abstract string Description { get; }
         public abstract Stream Icon { get; }
-        public string RepoName { get; }
-
-        public GithubReleaseModule(string repoName)
-        {
-            RepoName = repoName;
-        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public abstract T GetNewInstance();
 
-        public virtual async Task CheckForUpdates(bool includePrereleases)
+        public virtual Task CheckForUpdates(bool includePrereleases)
         {
-            var releases = await GithubApi.GetReleasesAsync(RepoName);
-
-
             throw new NotImplementedException();
         }
 
