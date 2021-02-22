ea14973c54096ad2848dbb104f567cdba39d4e7c
Caelan Sayler <git@caesay.com>
2021-02-22 10:56:18 +0000

    WIP add more DI
    Former-commit-id: ca214b840be16ec754a5c2341fdb717893a7b571

--- Clowd/Properties/AssemblyInfo.cs
+++ Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.4.47.2075")]
-[assembly: AssemblyFileVersion("2.4.47.2075")]
+[assembly: AssemblyVersion("2.4.47.2086")]
+[assembly: AssemblyFileVersion("2.4.47.2086")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
