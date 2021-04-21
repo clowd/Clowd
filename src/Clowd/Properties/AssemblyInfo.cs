0a582701104151b40cce92904144c0d317444f13
Caelan Sayler <git@caesay.com>
2021-04-21 10:41:55 +0100

    re-structure solution directories

--- src/Clowd/Properties/AssemblyInfo.cs
+++ src/Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.4.47.2088")]
-[assembly: AssemblyFileVersion("2.4.47.2088")]
+[assembly: AssemblyVersion("2.4.47.2091")]
+[assembly: AssemblyFileVersion("2.4.47.2091")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
