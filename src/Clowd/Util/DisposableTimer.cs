c3f30e7315d56d4c3c9740cc1bfe1284364831fc
Caelan Sayler <git@caesay.com>
2021-06-04 11:40:53 +0100

    add SynchronizationContext utility classes

---
+++ src/Clowd/Util/DisposableTimer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Clowd.Util
+{
+    public static class DisposableTimer
+    {
+        public static IDisposable Start(TimeSpan interval, Action action)
+        {
+            return Start(interval, action, true);
+        }
+        public static IDisposable Start(TimeSpan interval, Action action, bool synchronized)
+        {
+            var timer = new Timer();
+            if (synchronized)
+                timer.SynchronizingObject = new SynchronizationContextProxy();
+            timer.AutoReset = true;
+            timer.Interval = interval.TotalMilliseconds;
+            timer.Elapsed += (sender, args) => action();
+            timer.Start();
+
+            return new timerDisposer(timer);
+        }
+
+        private class timerDisposer : IDisposable
+        {
+            private Timer _timer;
+
+            public timerDisposer(Timer timer)
+            {
+                _timer = timer;
+            }
+
+            public void Dispose()
+            {
+                _timer?.Stop();
+                _timer?.Dispose();
+                _timer = null;
+            }
+        }
+    }
+}
