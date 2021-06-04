42508a6e8aad6707183069df8fc7ba91a2b99aac
Caelan Sayler <git@caesay.com>
2021-06-04 11:40:53 +0100

    add SynchronizationContext utility classes
    Former-commit-id: c3f30e7315d56d4c3c9740cc1bfe1284364831fc

---
+++ src/Clowd.Shared/SimpleNotifyObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowd
+{
+    public abstract class SimpleNotifyObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void OnPropertiesChanged(params string[] propertyNames)
+        {
+            foreach (string propertyName in propertyNames)
+            {
+                OnPropertyChanged(propertyName);
+            }
+        }
+    }
+
+    public abstract class DictionaryNotifyObject : SimpleNotifyObject
+    {
+        private Dictionary<string, object> _store = new Dictionary<string, object>();
+
+        protected bool SetProperty<T>(T value, [CallerMemberName] string propertyName = null)
+        {
+            if (_store.TryGetValue(propertyName, out var stor) && Equals(stor, value))
+                return false;
+
+            _store[propertyName] = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected T GetProperty<T>([CallerMemberName] string propertyName = null)
+        {
+            if (_store.TryGetValue(propertyName, out var stor))
+                if (stor.GetType().IsAssignableFrom(typeof(T)))
+                    return (T)stor;
+
+            return default;
+        }
+    }
+}
