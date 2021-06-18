b6e7b5964706ebac25619434a0a83c84e6f6838c
Caelan Sayler <git@caesay.com>
2021-06-18 17:57:57 +0100

    Fix bug where full screen borderless windows are being ignored by capture window detection

--- src/Clowd/Properties/AssemblyInfo.cs
+++ src/Clowd/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 
 // Version Information
 // Revision and build numbers are auto-incrimented.
-[assembly: AssemblyVersion("2.4.65.3243")]
-[assembly: AssemblyFileVersion("2.4.65.3243")]
+[assembly: AssemblyVersion("2.4.65.3244")]
+[assembly: AssemblyFileVersion("2.4.65.3244")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 // Assembly Information
