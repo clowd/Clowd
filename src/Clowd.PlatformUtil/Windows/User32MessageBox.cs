48faf391f9e9c692334833c57a4daf7922b68a6c
Caelan <git@caesay.com>
2021-07-07 17:42:02 +0100

    More message box button options
    Former-commit-id: 9c2160f3bbaae77451c64d6e03b3c72dac2e4c6f

--- src/Clowd.PlatformUtil/Windows/User32MessageBox.cs
+++ src/Clowd.PlatformUtil/Windows/User32MessageBox.cs
@@ -7,7 +7,7 @@ namespace Clowd.PlatformUtil.Windows
     public static class User32MessageBox
     {
         [DllImport("User32", EntryPoint = "MessageBox", SetLastError = true, CharSet = CharSet.Auto, BestFitMapping = false)]
-        private static extern int MessageBoxShow(HandleRef hWnd, string text, string caption, int type);
+        private static extern int MessageBoxShow(IntPtr hWnd, string text, string caption, int type);
 
         private const int IDOK = 1;
         private const int IDCANCEL = 2;
@@ -21,8 +21,8 @@ namespace Clowd.PlatformUtil.Windows
             IntPtr owner,
             string messageBoxText,
             string caption,
-            MessageBoxButton button,
-            MessageBoxImage icon, MessageBoxResult defaultResult,
+            MessageBoxButtons button,
+            MessageBoxIcon icon, MessageBoxResult defaultResult,
             MessageBoxOptions options)
         {
             return ShowCore(owner, messageBoxText, caption, button, icon, defaultResult, options);
@@ -32,8 +32,8 @@ namespace Clowd.PlatformUtil.Windows
             IntPtr owner,
             string messageBoxText,
             string caption,
-            MessageBoxButton button,
-            MessageBoxImage icon,
+            MessageBoxButtons button,
+            MessageBoxIcon icon,
             MessageBoxResult defaultResult)
         {
             return ShowCore(owner, messageBoxText, caption, button, icon, defaultResult, 0);
@@ -43,8 +43,8 @@ namespace Clowd.PlatformUtil.Windows
             IntPtr owner,
             string messageBoxText,
             string caption,
-            MessageBoxButton button,
-            MessageBoxImage icon)
+            MessageBoxButtons button,
+            MessageBoxIcon icon)
         {
             return ShowCore(owner, messageBoxText, caption, button, icon, 0, 0);
         }
@@ -53,46 +53,35 @@ namespace Clowd.PlatformUtil.Windows
             IntPtr owner,
             string messageBoxText,
             string caption,
-            MessageBoxButton button)
+            MessageBoxButtons button)
         {
-            return ShowCore(owner, messageBoxText, caption, button, MessageBoxImage.None, 0, 0);
+            return ShowCore(owner, messageBoxText, caption, button, MessageBoxIcon.None, 0, 0);
         }
 
         public static MessageBoxResult Show(IntPtr owner, string messageBoxText, string caption)
         {
-            return ShowCore(owner, messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.None, 0, 0);
+            return ShowCore(owner, messageBoxText, caption, MessageBoxButtons.OK, MessageBoxIcon.None, 0, 0);
         }
 
         public static MessageBoxResult Show(IntPtr owner, string messageBoxText)
         {
-            return ShowCore(owner, messageBoxText, String.Empty, MessageBoxButton.OK, MessageBoxImage.None, 0, 0);
+            return ShowCore(owner, messageBoxText, String.Empty, MessageBoxButtons.OK, MessageBoxIcon.None, 0, 0);
         }
 
         internal static MessageBoxResult ShowCore(
             IntPtr owner,
             string messageBoxText,
             string caption,
-            MessageBoxButton button,
-            MessageBoxImage icon,
+            MessageBoxButtons button,
+            MessageBoxIcon icon,
             MessageBoxResult defaultResult,
             MessageBoxOptions options)
         {
-            if (!IsValidMessageBoxButton(button))
-            {
-                throw new InvalidEnumArgumentException("button", (int)button, typeof(MessageBoxButton));
-            }
-            if (!IsValidMessageBoxImage(icon))
-            {
-                throw new InvalidEnumArgumentException("icon", (int)icon, typeof(MessageBoxImage));
-            }
-            if (!IsValidMessageBoxResult(defaultResult))
-            {
-                throw new InvalidEnumArgumentException("defaultResult", (int)defaultResult, typeof(MessageBoxResult));
-            }
-            if (!IsValidMessageBoxOptions(options))
-            {
-                throw new InvalidEnumArgumentException("options", (int)options, typeof(MessageBoxOptions));
-            }
+
+            // for positioning message box and custom button labels, see here
+            // https://www.codeguru.com/cpp/w-p/win32/messagebox/article.php/c10873/MessageBox-with-Custom-Button-Captions.htm
+            // need a CBT hook
+            // https://stackoverflow.com/questions/1530561/set-location-of-messagebox
 
             if ((options & (MessageBoxOptions.ServiceNotification | MessageBoxOptions.DefaultDesktopOnly)) != 0)
             {
@@ -101,99 +90,33 @@ namespace Clowd.PlatformUtil.Windows
                     throw new ArgumentException("Can't show a service notification with an owner.");
                 }
             }
-            else
-            {
-                if (owner == IntPtr.Zero)
-                {
-                    throw new ArgumentException("Must have a valid owner window.");
-                }
-            }
 
             int style = (int)button | (int)icon | (int)DefaultResultToButtonNumber(defaultResult, button) | (int)options;
 
-            return Win32ToMessageBoxResult(MessageBoxShow(new HandleRef(null, owner), messageBoxText, caption, style));
+            return (MessageBoxResult)(MessageBoxShow(owner, messageBoxText, caption, style));
         }
 
-        private static int DefaultResultToButtonNumber(MessageBoxResult result, MessageBoxButton button)
+        private static int DefaultResultToButtonNumber(MessageBoxResult result, MessageBoxButtons button)
         {
             if (result == 0) return DEFAULT_BUTTON1;
 
             switch (button)
             {
-                case MessageBoxButton.OK:
+                case MessageBoxButtons.OK:
                     return DEFAULT_BUTTON1;
-                case MessageBoxButton.OKCancel:
+                case MessageBoxButtons.OKCancel:
                     if (result == MessageBoxResult.Cancel) return DEFAULT_BUTTON2;
                     return DEFAULT_BUTTON1;
-                case MessageBoxButton.YesNo:
+                case MessageBoxButtons.YesNo:
                     if (result == MessageBoxResult.No) return DEFAULT_BUTTON2;
                     return DEFAULT_BUTTON1;
-                case MessageBoxButton.YesNoCancel:
+                case MessageBoxButtons.YesNoCancel:
                     if (result == MessageBoxResult.No) return DEFAULT_BUTTON2;
                     if (result == MessageBoxResult.Cancel) return DEFAULT_BUTTON3;
                     return DEFAULT_BUTTON1;
                 default:
                     return DEFAULT_BUTTON1;
             }
-        }
-
-        private static MessageBoxResult Win32ToMessageBoxResult(int value)
-        {
-            switch (value)
-            {
-                case IDOK:
-                    return MessageBoxResult.OK;
-                case IDCANCEL:
-                    return MessageBoxResult.Cancel;
-                case IDYES:
-                    return MessageBoxResult.Yes;
-                case IDNO:
-                    return MessageBoxResult.No;
-                default:
-                    return MessageBoxResult.No;
-            }
-        }
-
-        private static bool IsValidMessageBoxButton(MessageBoxButton value)
-        {
-            return value == MessageBoxButton.OK
-                || value == MessageBoxButton.OKCancel
-                || value == MessageBoxButton.YesNo
-                || value == MessageBoxButton.YesNoCancel;
-        }
-
-        private static bool IsValidMessageBoxImage(MessageBoxImage value)
-        {
-            return value == MessageBoxImage.Asterisk
-                || value == MessageBoxImage.Error
-                || value == MessageBoxImage.Exclamation
-                || value == MessageBoxImage.Hand
-                || value == MessageBoxImage.Information
-                || value == MessageBoxImage.None
-                || value == MessageBoxImage.Question
-                || value == MessageBoxImage.Stop
-                || value == MessageBoxImage.Warning;
-        }
-
-        private static bool IsValidMessageBoxResult(MessageBoxResult value)
-        {
-            return value == MessageBoxResult.Cancel
-                || value == MessageBoxResult.No
-                || value == MessageBoxResult.None
-                || value == MessageBoxResult.OK
-                || value == MessageBoxResult.Yes;
-        }
-
-        private static bool IsValidMessageBoxOptions(MessageBoxOptions value)
-        {
-            int mask = ~((int)MessageBoxOptions.ServiceNotification |
-                         (int)MessageBoxOptions.DefaultDesktopOnly |
-                         (int)MessageBoxOptions.RightAlign |
-                         (int)MessageBoxOptions.RtlReading);
-
-            if (((int)value & mask) == 0)
-                return true;
-            return false;
         }
     }
 
