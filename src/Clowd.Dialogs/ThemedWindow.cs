f47631b472283f1bc85dcfcbdc02ea9a297267bc
Caelan Sayler <git@caesay.com>
2021-07-08 18:56:25 +0100

    Playing with dark mode dialogs
    Former-commit-id: 5b838c9f5e28ae0a73c71df9a808cf2a1e14fa8d

---
+++ src/Clowd.Dialogs/ThemedWindow.cs
@@ -0,0 +1,77 @@
+﻿using Clowd.PlatformUtil.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clowd.Dialogs
+{
+    public class ThemedWindow : Window
+    {
+        public static readonly ResourceDictionary LightTheme = new ResourceDictionary()
+        {
+            Source = new Uri("/Clowd.Dialogs;component/Themes/Light/LightTheme.xaml", UriKind.RelativeOrAbsolute)
+        };
+
+        public static readonly ResourceDictionary DarkTheme = new ResourceDictionary()
+        {
+            Source = new Uri("/Clowd.Dialogs;component/Themes/Dark/DarkTheme.xaml", UriKind.RelativeOrAbsolute)
+        };
+
+        private bool? _isDark;
+
+        public ThemedWindow()
+        {
+            Resources.MergedDictionaries.Add(LightTheme);
+        }
+
+        protected virtual IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == (int)WindowMessage.WM_SETTINGCHANGE)
+                UpdateDarkModeState(hwnd);
+            return IntPtr.Zero;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            var handle = new WindowInteropHelper(this).EnsureHandle();
+            UpdateDarkModeState(handle);
+            HwndSource source = HwndSource.FromHwnd(handle);
+            source.AddHook(new HwndSourceHook(WndProc));
+            UpdateDarkModeState(handle);
+            base.OnSourceInitialized(e);
+        }
+
+        protected virtual void UpdateDarkModeState(IntPtr handle)
+        {
+            var systemIsDark = DarkMode.IsDarkModeEnabled();
+            if (systemIsDark && _isDark != true)
+            {
+                DarkMode.UseImmersiveDarkMode(handle, true);
+                Resources.MergedDictionaries.Add(DarkTheme);
+                Background = Brushes.Black;
+                _isDark = true;
+            }
+            else if (!systemIsDark && _isDark != false)
+            {
+                DarkMode.UseImmersiveDarkMode(handle, false);
+
+                if (Resources.MergedDictionaries.Contains(DarkTheme))
+                    Resources.MergedDictionaries.Remove(DarkTheme);
+
+                Background = Brushes.White;
+                _isDark = false;
+            }
+        }
+    }
+}
