3f2afafb303754822ec8a018dcf820b676d3d2ca
Caelan Sayler <git@caesay.com>
2021-07-11 15:51:42 +0100

    Add font picker

---
+++ src/Clowd.Dialogs/Font/ColorPicker.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clowd.Dialogs.Font
+{
+    /// <summary>
+    /// Interaction logic for ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl
+    {
+        private ColorPickerViewModel viewModel;
+
+        public readonly static RoutedEvent ColorChangedEvent;
+
+        public readonly static DependencyProperty SelectedColorProperty;
+
+        public FontColor SelectedColor
+        {
+            get
+            {
+                FontColor fc = (FontColor)base.GetValue(ColorPicker.SelectedColorProperty) ?? AvailableColors.GetFontColor("Black");
+                return fc;
+            }
+            set
+            {
+                this.viewModel.SelectedFontColor = value;
+                base.SetValue(ColorPicker.SelectedColorProperty, value);
+            }
+        }
+
+        static ColorPicker()
+        {
+            ColorPicker.ColorChangedEvent = EventManager.RegisterRoutedEvent("ColorChanged", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ColorPicker));
+            ColorPicker.SelectedColorProperty = DependencyProperty.Register("SelectedColor", typeof(FontColor), typeof(ColorPicker), new UIPropertyMetadata(null));
+        }
+        public ColorPicker()
+        {
+            InitializeComponent();
+            this.viewModel = new ColorPickerViewModel();
+            base.DataContext = this.viewModel;
+        }
+        private void RaiseColorChangedEvent()
+        {
+            base.RaiseEvent(new RoutedEventArgs(ColorPicker.ColorChangedEvent));
+        }
+
+        private void superCombo_DropDownClosed(object sender, EventArgs e)
+        {
+            base.SetValue(ColorPicker.SelectedColorProperty, this.viewModel.SelectedFontColor);
+            this.RaiseColorChangedEvent();
+        }
+
+        private void superCombo_Loaded(object sender, RoutedEventArgs e)
+        {
+            base.SetValue(ColorPicker.SelectedColorProperty, this.viewModel.SelectedFontColor);
+        }
+
+        public event RoutedEventHandler ColorChanged
+        {
+            add
+            {
+                base.AddHandler(ColorPicker.ColorChangedEvent, value);
+            }
+            remove
+            {
+                base.RemoveHandler(ColorPicker.ColorChangedEvent, value);
+            }
+        }
+    }
+}
---
+++ src/Clowd.Dialogs/Font/FontValueConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Clowd.Dialogs.Font
+{
+    public class FontValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is FontFamily font)
+            {
+                //var name = I18NUtil.CurrentLanguage;
+                //if (string.IsNullOrWhiteSpace(name))
+                //{
+                //    name = I18NUtil.GetCurrentLanguage();
+                //}
+                //var names = new[] { name, I18NUtil.GetLanguage() };
+                //foreach (var s in names)
+                //{
+                //    if (font.FamilyNames.TryGetValue(XmlLanguage.GetLanguage(s), out var localizedName))
+                //    {
+                //        if (!string.IsNullOrEmpty(localizedName))
+                //        {
+                //            return localizedName;
+                //        }
+                //    }
+                //}
+
+                return font.Source;
+            }
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack not supported");
+        }
+    }
+}
