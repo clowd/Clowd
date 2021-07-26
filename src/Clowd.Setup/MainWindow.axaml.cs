abb48a8361615f1a27b1985750d314aabc34e111
Caelan Sayler <git@caesay.com>
2021-07-26 20:35:34 +0100

    WIP UI installer
    Former-commit-id: 3989876a2cb1aed1725acb31d9ab47a8d2b6ac17

---
+++ src/Clowd.Setup/MainWindow.axaml.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Clowd.Installer;
+using Clowd.Installer.Features;
+using Clowd.Setup.Views;
+
+namespace Clowd.Setup
+{
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Current { get; private set; }
+        public Border ContentView => this.FindControl<Border>("ContentView");
+
+        public MainWindow()
+        {
+            Current = this;
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+            var info = ControlPanelInfo.GetInfo("Clowd", RegistryQuery.CurrentUser);
+            ContentView.Child = info == null ? new InstallView() : null;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public void SetContent(IControl control)
+        {
+            ContentView.Child = control;
+        }
+    }
+}
