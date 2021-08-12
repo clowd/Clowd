1db93b13b85bc0f9906cd7b6897843b577a5b1f9
Caelan Sayler <git@caesay.com>
2021-08-12 22:31:15 +0100

    WIP; Add install/update with Squirrel
    Former-commit-id: 324c7850a7c309156aa1c9ff01a761af6cd613eb

--- publish.ps1
+++ publish.ps1
@@ -1,13 +1,28 @@
+Set-Location "$PSScriptRoot"
+$ErrorActionPreference = "Stop"
+
+# Ensure a clean state by removing build/package folders
+$Folders = @("$PSScriptRoot\publish", "$PSScriptRoot\bin")
+foreach ($Folder in $Folders) {
+    if (Test-Path $Folder) {
+        Remove-Item -path "$Folder" -Recurse -Force
+    }
+}
+
+# Get msbuild location
 $MSBuildPath = (&"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe) | Out-String
 $MSBuildPath = $MSBuildPath.Trim();
 
-Remove-Item "$PSScriptRoot\publish" -Recurse
+# calculate version
+$gitVerJson = (&nbgv get-version -f json) | ConvertFrom-Json
+$version = $gitVerJson.SimpleVersion
 
+# publish Clowd project
 &$MSBuildPath "$PSScriptRoot\src\Clowd\Clowd.csproj" `
 /t:Restore,Rebuild,Publish `
 /v:minimal `
 /p:PublishSingleFile=False `
-/p:SelfContained=True `
+/p:SelfContained=False `
 /p:PublishProtocol=FileSystem `
 /p:Configuration=Release `
 /p:Platform=x64 `
@@ -16,20 +31,42 @@ Remove-Item "$PSScriptRoot\publish" -Recurse
 /p:PublishReadyToRun=False `
 /p:PublishTrimmed=False `
 /p:SolutionDir=$PSScriptRoot 
-# /p:AllowedReferenceRelatedFileExtensions=none
 
-# &$MSBuildPath "$PSScriptRoot\src\Clowd.Installer\Clowd.Installer.csproj" `
-# /t:Restore,Rebuild,Publish `
-# /v:minimal `
-# /p:PublishSingleFile=True `
-# /p:SelfContained=True `
-# /p:PublishProtocol=FileSystem `
-# /p:Configuration=Release `
-# /p:Platform=AnyCPU `
-# /p:PublishDir=$PSScriptRoot\publish\ClowdCLI `
-# /p:RuntimeIdentifier=win-x64 `
-# /p:PublishReadyToRun=False `
-# /p:PublishTrimmed=True `
-# /p:SolutionDir=$PSScriptRoot `
-# /p:AllowedReferenceRelatedFileExtensions=none `
-# /p:IncludeNativeLibrariesForSelfExtract=True
+# build packaging tools
+# powershell -ExecutionPolicy Bypass -File "$PSScriptRoot\modules\Clowd.Squirrel\build.ps1"
+
+# create nuget package
+&$PSScriptRoot\tools\NuGet.exe pack "$PSScriptRoot\src\Clowd\Clowd.nuspec" `
+-BasePath "$PSScriptRoot\publish\Clowd\" `
+-OutputDirectory "$PSScriptRoot\publish" `
+-Version $version
+
+# releasify
+&$PSScriptRoot\tools\Squirrel.com --releasify "$PSScriptRoot\publish\Clowd.$($version).nupkg" `
+--setupIcon="$PSScriptRoot\src\Clowd\Images\default.ico" `
+--no-msi `
+--releaseDir="$PSScriptRoot\releases"
+
+# # Set-Location "$PSScriptRoot\publish\Clowd"
+# # &$PSScriptRoot\tools\NuGet.exe spec Clowd.dll
+
+# # &$MSBuildPath "$PSScriptRoot\src\Clowd\Clowd.csproj" `
+# # /t:Pack `
+# # /p:NoBuild=true `
+# # /p:IncludeBuildOutput=false `
+# # /p:PackageVersion="1.3" `
+# # /p:PackageOutputPath="$PSScriptRoot\publish" `
+# # /p:NuspecBasePath="$PSScriptRoot\publish\Clowd" `
+# # /p:NuspecFile="$PSScriptRoot\src\Clowd\Clowd.nuspec"
+
+
+
+# &$PSScriptRoot\tools\NuGet.exe pack "$PSScriptRoot\src\Clowd\Clowd.nuspec" `
+# -BasePath "$PSScriptRoot\publish\Clowd\" `
+# -OutputDirectory "$PSScriptRoot\publish" `
+# -Version $Ver
+
+# &$PSScriptRoot\tools\Squirrel.com --releasify "$PSScriptRoot\publish\Clowd.$($Ver).nupkg" `
+# --setupIcon="$PSScriptRoot\src\Clowd\Images\default.ico" `
+# --no-msi `
+# --releaseDir="$PSScriptRoot\releases"
