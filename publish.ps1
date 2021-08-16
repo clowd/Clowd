5f9918c7ee6cf697f3777ee083ac51886a184c18
Caelan Sayler <git@caesay.com>
2021-08-16 09:30:08 +0100

    Bundle obs-express into clowd packages
    Former-commit-id: d3b978f74f30644f15586ac1c36821e4fdaeac08

--- publish.ps1
+++ publish.ps1
@@ -2,7 +2,7 @@ Set-Location "$PSScriptRoot"
 $ErrorActionPreference = "Stop"
 
 # Ensure a clean state by removing build/package folders
-$Folders = @("$PSScriptRoot\publish", "$PSScriptRoot\bin")
+$Folders = @("$PSScriptRoot\publish", "$PSScriptRoot\bin", "$PSScriptRoot\releases")
 foreach ($Folder in $Folders) {
     if (Test-Path $Folder) {
         Remove-Item -path "$Folder" -Recurse -Force
@@ -14,12 +14,12 @@ $MSBuildPath = (&"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vsw
 $MSBuildPath = $MSBuildPath.Trim();
 
 # calculate version
-Write-Host "Get Clowd Version"
+Write-Host "Get Clowd Version" -ForegroundColor Magenta
 $gitVerJson = (&nbgv get-version -f json) | ConvertFrom-Json
 $version = $gitVerJson.SimpleVersion
 
 # publish Clowd project
-Write-Host "Build Clowd"
+Write-Host "Build Clowd" -ForegroundColor Magenta
 &$MSBuildPath "$PSScriptRoot\src\Clowd\Clowd.csproj" `
 /t:Restore,Rebuild,Publish `
 /v:minimal `
@@ -34,25 +34,34 @@ Write-Host "Build Clowd"
 /p:PublishTrimmed=False `
 /p:SolutionDir=$PSScriptRoot 
 
+# build obs-express
+Write-Host "Build obs-express" -ForegroundColor Magenta
+Set-Location "$PSScriptRoot\modules\obs-express"
+&npm install
+&npm run build
+Copy-Item "bin" -Destination "$PSScriptRoot\publish\Clowd\obs-express" -Recurse
+
 # build packaging tools
-Write-Host "Build Packaging Tools"
+Write-Host "Build Packaging Tools" -ForegroundColor Magenta
 Set-Location "$PSScriptRoot\modules\Clowd.Squirrel"
 Copy-Item "$PSScriptRoot\default-setup.ico" "src\Update\ins-update.ico"
 & ".\build.cmd"
 Set-Location "$PSScriptRoot"
 
 # create nuget package
-Write-Host "Create Nuget Package"
+Write-Host "Create Nuget Package" -ForegroundColor Magenta
 & $PSScriptRoot\modules\Clowd.Squirrel\build\publish\NuGet.exe pack "$PSScriptRoot\src\Clowd\Clowd.nuspec" `
 -BasePath "$PSScriptRoot\publish\Clowd\" `
 -OutputDirectory "$PSScriptRoot\publish" `
 -Version $version
 
 # releasify
-Write-Host "Releasify Nuget Package"
+Write-Host "Releasify Nuget Package" -ForegroundColor Magenta
 & $PSScriptRoot\modules\Clowd.Squirrel\build\publish\Squirrel --releasify "$PSScriptRoot\publish\Clowd.$($version).nupkg" `
 --setupIcon="$PSScriptRoot\default-setup.ico" `
 --no-msi `
 --releaseDir="$PSScriptRoot\releases"
 
-Write-Host "Done"
+# upload new release
+
+Write-Host "Done" -ForegroundColor Magenta
