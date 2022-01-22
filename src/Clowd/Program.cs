5533281aa64a509874f2e8e4a4e83f24aa59c014
Caelan Sayler <git@caesay.com>
2022-01-22 10:53:53 +0000

    New entry fn to process 'watch' command before WPF even starts
    Former-commit-id: 0c87ee462e2577aee99bfaf9e10e9377064d78d9

---
+++ src/Clowd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowd
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            // fast exit for "watch" command
+            if (args.Length > 0 && args[0].Equals("watch", StringComparison.InvariantCulture))
+            {
+                ProcessWatcher.Run(args.Skip(1).ToArray());
+                return;
+            }
+
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
