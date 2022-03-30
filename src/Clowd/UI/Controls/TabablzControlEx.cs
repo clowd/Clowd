8b6d0f3ec4b4f64a49be7bc137559461ec0f5f4c
Caelan Sayler <git@caesay.com>
2022-03-30 21:49:01 +0100

    Add Dragablz back
    Former-commit-id: e82c9aa2ee679ecd241bb1b8f278fcb89a2f3a98

---
+++ src/Clowd/UI/Controls/TabablzControlEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Dragablz;
+
+namespace Clowd.UI.Controls
+{
+    public class TabablzControlEx : TabablzControl
+    {
+        public event EventHandler ItemsChanged;
+
+        static TabablzControlEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TabablzControlEx), new FrameworkPropertyMetadata(typeof(TabablzControlEx)));
+        }
+
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+            ItemsChanged?.Invoke(this, e);
+        }
+    }
+}
