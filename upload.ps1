6ca924428a99b4e5ebf8355b20517f94e3b3beb7
Caelan <git@caesay.com>
2022-04-08 12:03:32 +0100

    update Upload script to use new Squirrel location
    Former-commit-id: 23d831e45e9c4d356a678121c90051f66c8228bf

--- upload.ps1
+++ upload.ps1
@@ -1,5 +1,20 @@
 Set-Location "$PSScriptRoot"
 $ErrorActionPreference = "Stop"
+$LocalProjectMode = Test-Path -Path "$PSScriptRoot\.usingproj" -PathType Leaf
+
+# build packaging tools only if in local mode
+if ($LocalProjectMode) {
+    Write-Host "Squirrel (local mode)" -ForegroundColor Magenta
+    Set-Alias Squirrel ("$PSScriptRoot\..\Clowd.Squirrel\build\publish\Squirrel.exe");
+} else {
+    $projdeps = dotnet list "$PSScriptRoot\src\Clowd\Clowd.csproj" package
+    $projstring = $projdeps -join "`r`n"
+    $squirrelVersion = $projstring -match "(?m)Clowd\.Squirrel.*\s(\d\.\d\.\d.*?)$"
+    $squirrelVersion = $Matches[1].Trim()
+    Write-Host "Using Squirrel '$squirrelVersion' from local NuGet cache" -ForegroundColor Magenta
+    Set-Alias Squirrel ($env:USERPROFILE + "\.nuget\packages\clowd.squirrel\$squirrelVersion\tools\Squirrel.exe");
+}
+Set-Location $PSScriptRoot
 
 if (Test-Path '.\clowd_secrets.json') {
     $secrets = Get-Content '.\clowd_secrets.json' | Out-String | ConvertFrom-Json
@@ -10,7 +25,7 @@ if (Test-Path '.\clowd_secrets.json') {
 }
 
 Write-Host "Upload latest releases" -ForegroundColor Magenta
-& $PSScriptRoot\modules\Clowd.Squirrel\build\publish\Squirrel.exe b2-up `
+Squirrel b2-up `
 -r "$PSScriptRoot\releases" `
 --b2BucketId $secrets.b2bucket `
 --b2keyid $secrets.b2keyid `
