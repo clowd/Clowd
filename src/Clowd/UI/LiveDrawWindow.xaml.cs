63fef1eb6a9374bf10dc04b8b84d5c982aa6762a
Caelan <git@caesay.com>
2022-05-30 22:14:08 -0600

    Move LiveDraw into main Dll

--- src/Clowd/UI/LiveDrawWindow.xaml.cs
+++ src/Clowd/UI/LiveDrawWindow.xaml.cs
@@ -1,26 +1,16 @@
-﻿using Clowd;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Ink;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Threading;
 using Brush = System.Windows.Media.Brush;
 using Point = System.Windows.Point;
 
-namespace AntFu7.LiveDraw
+namespace Clowd.UI
 {
     public partial class LiveDrawWindow : Window, ILiveDrawPage
     {
