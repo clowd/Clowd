e750ef71ed48d41bc2690de70fd69de857e9f433
Roman Starkov <sourcecode@roman.st>
2022-06-01 23:29:09 +0100

    Can't use InputBinding directly; add helper
    Former-commit-id: f4298a95cfb38bbb2ae8d6f5bdb5b605a22a2837

--- src/Clowd.Shared/BareKeyGesture.cs
+++ src/Clowd.Shared/BareKeyGesture.cs
@@ -25,4 +25,24 @@ namespace Clowd
             return (int)Key == (int)keyEventArgs.Key && Keyboard.Modifiers == ModifierKeys.None;
         }
     }
+
+    /// <summary>
+    /// This only exists because the InputBinding constructor is protected, but since we have to have it anyway
+    /// we also use this opportunity to simplify adding a BareKeyGesture to it.
+    /// </summary>
+    public class BareKeyBinding : InputBinding
+    {
+        private BareKeyGesture _gesture = new();
+
+        public BareKeyBinding()
+        {
+            Gesture = _gesture;
+        }
+
+        public Key Key
+        {
+            get => _gesture.Key;
+            set { _gesture.Key = value; }
+        }
+    }
 }
