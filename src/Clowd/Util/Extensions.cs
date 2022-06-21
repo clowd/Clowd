986bcbf6958fad79a81c94975e41ee5ba02aa879
Caelan <git@caesay.com>
2022-06-20 23:05:05 -0600

    CommonDialog's need to be run in an STA thread
    Former-commit-id: 508e79524baab896153ced14f45982a2b7ed3b1a

--- src/Clowd/Util/Extensions.cs
+++ src/Clowd/Util/Extensions.cs
@@ -417,5 +417,18 @@ namespace Clowd.Util
                     yield return childOfChild;
             }
         }
+        
+        public static Task WaitOneAsync(this WaitHandle waitHandle)
+        {
+            if (waitHandle == null)
+                throw new ArgumentNullException("waitHandle");
+
+            var tcs = new TaskCompletionSource<bool>();
+            var rwh = ThreadPool.RegisterWaitForSingleObject(waitHandle,
+                delegate { tcs.TrySetResult(true); }, null, -1, true);
+            var t = tcs.Task;
+            t.ContinueWith( (antecedent) => rwh.Unregister(null));
+            return t;
+        }
     }
 }
