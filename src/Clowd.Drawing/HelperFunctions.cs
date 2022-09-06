93ab76a7225a790ba6daff60ffa36c92021526ed
Caelan Sayler <git@caesay.com>
2022-09-06 22:59:49 +0100

    Support snapping rectangles and lines during initial draw and subsequent resizing

--- src/Clowd.Drawing/HelperFunctions.cs
+++ src/Clowd.Drawing/HelperFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using RT.Util.Geometry;
 
 
 namespace Clowd.Drawing
@@ -42,6 +43,34 @@ namespace Clowd.Drawing
         {
             var r = CreateRectSafe(Left, Top, Right, Bottom);
             return new Rect(Math.Round(r.Left), Math.Round(r.Top), Math.Round(r.Width), Math.Round(r.Height));
+        }
+
+        public static Point SnapPointToCommonAngle(Point anchor, Point point, bool diagOnly)
+        {
+            double x1 = anchor.X, y1 = anchor.Y, x2 = point.X, y2 = point.Y;
+            double xDiff = x2 - x1;
+            double yDiff = y2 - y1;
+
+            double closest45;
+
+            if (diagOnly)
+            {
+                var angle = (Math.Atan2(yDiff, xDiff) * 180.0 / Math.PI + 360 + 45) % 360;
+                closest45 = Math.Round(angle / 90d) * 90d - 45;
+            }
+            else
+            {
+                var angle = (Math.Atan2(yDiff, xDiff) * 180.0 / Math.PI + 360) % 360;
+                closest45 = Math.Round(angle / 45d) * 45d;
+            }
+            
+            var vecSnap = new PointD(closest45 / 180 * Math.PI);
+            var snapLen = new PointD(xDiff, yDiff).LengthProjectedOnto(vecSnap);
+            
+            var ox = anchor.X + (snapLen * vecSnap).X;
+            var oy = anchor.Y + (snapLen * vecSnap).Y;
+
+            return new Point(ox, oy);
         }
     }
 }
--- src/Clowd.Drawing/Tools/ToolDraggable.cs
+++ src/Clowd.Drawing/Tools/ToolDraggable.cs
@@ -2,28 +2,38 @@
 using System.Windows;
 using System.Windows.Input;
 using Clowd.Drawing.Graphics;
+using RT.Util.Geometry;
 
 namespace Clowd.Drawing.Tools
 {
-    internal class ToolDraggable<T> : ToolBase
-        where T : GraphicBase
+    enum DraggableSnapMode
+    {
+        None = 0,
+        Diagonal = 1,
+        All = 2,
+    }
+
+    internal class ToolDraggable<T> : ToolBase where T : GraphicBase
     {
         private readonly Func<Point, T> _create;
         private readonly Action<Point, T> _update;
         private readonly Action<T> _end;
+        private readonly DraggableSnapMode _snapMode;
 
-        internal T _instance;
+        private T _instance;
+        private Point _start;
 
         public ToolDraggable(Cursor cursor, Func<Point, T> create, Action<Point, T> update)
             : this(cursor, create, update, null)
         { }
 
-        public ToolDraggable(Cursor cursor, Func<Point, T> create, Action<Point, T> update, Action<T> end)
+        public ToolDraggable(Cursor cursor, Func<Point, T> create, Action<Point, T> update, Action<T> end = null, DraggableSnapMode snapMode = DraggableSnapMode.None)
             : base(cursor)
         {
             _create = create;
             _update = update;
             _end = end;
+            _snapMode = snapMode;
         }
 
         internal override ToolActionType ActionType => ToolActionType.Object;
@@ -32,25 +42,39 @@ namespace Clowd.Drawing.Tools
         {
             base.OnMouseDown(canvas, e);
             Point point = e.GetPosition(canvas);
+            _start = point;
             _instance = _create(point);
             _instance.IsSelected = true;
             canvas.GraphicsList.Add(_instance);
         }
 
+        private Point GetPoint(DrawingCanvas canvas, MouseEventArgs e)
+        {
+            // snap the point to a 45deg angle (maybe)
+            if (_snapMode != DraggableSnapMode.None && (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.LeftShift)))
+            {
+                return HelperFunctions.SnapPointToCommonAngle(_start, e.GetPosition(canvas), _snapMode == DraggableSnapMode.Diagonal);
+            }
+            
+            return e.GetPosition(canvas);
+        }
+
         public override void OnMouseMove(DrawingCanvas canvas, MouseEventArgs e)
         {
             base.OnMouseMove(canvas, e);
-            Point point = e.GetPosition(canvas);
             if (_instance != null)
+            {
+                Point point = GetPoint(canvas, e);
                 _update?.Invoke(point, _instance);
+            }
         }
 
         public override void OnMouseUp(DrawingCanvas canvas, MouseButtonEventArgs e)
         {
             base.OnMouseUp(canvas, e);
-            Point point = e.GetPosition(canvas);
             if (_instance != null)
             {
+                Point point = GetPoint(canvas, e);
                 _update?.Invoke(point, _instance);
                 _end?.Invoke(_instance);
                 _instance.Normalize();
