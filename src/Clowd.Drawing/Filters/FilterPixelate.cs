d2a92e7291c1df9077772e1b638cf7cb36c2e799
Caelan Sayler <git@caesay.com>
2022-09-09 01:03:28 +0100

    Obscure tool is now a selection rectangle.

--- src/Clowd.Drawing/Filters/FilterPixelate.cs
+++ src/Clowd.Drawing/Filters/FilterPixelate.cs
@@ -1,210 +1,210 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using Clowd.Drawing.Graphics;
-using Clowd.PlatformUtil;
-
-namespace Clowd.Drawing.Filters
-{
-    internal class FilterPixelate : FilterBase
-    {
-        private DrawingCanvas _canvas;
-        private GraphicImage _source;
-        private BitmapSource _imageSource;
-        private RenderTargetBitmap _imageOverlay;
-        private byte[] _imageBytes;
-        private int _imageStride;
-        private Image _rendered;
-        private ScreenSize _originalSize;
-        private bool _opened;
-
-        public override bool Start(DrawingBrush brush, Point p, DrawingCanvas canvas)
-        {
-            var clicked = canvas.ToolPointer.MakeHitTest(canvas, p, out var handleNum);
-            if (clicked is not GraphicImage source)
-                return false;
-
-            _canvas = canvas;
-            _source = source;
-            var image = CachedBitmapLoader.LoadFromFile(source.BitmapFilePath);
-            _originalSize = new ScreenSize(image.PixelWidth, image.PixelHeight);
-            image = new CroppedBitmap(image, source.Crop);
-
-            // make sure image is in the correct format.
-            if (image.Format != PixelFormats.Bgra32)
-            {
-                FormatConvertedBitmap formatted = new FormatConvertedBitmap();
-                formatted.BeginInit();
-                formatted.Source = image;
-                formatted.DestinationFormat = PixelFormats.Bgra32;
-                formatted.EndInit();
-                image = formatted;
-            }
-
-            int nStride = (image.PixelWidth * image.Format.BitsPerPixel + 7) / 8;
-            byte[] pixelByteArray = new byte[image.PixelHeight * nStride];
-            image.CopyPixels(pixelByteArray, nStride, 0);
-            _imageBytes = pixelByteArray;
-            _imageStride = nStride;
-            _imageSource = image;
-
-            // load any previous pixelations into the current overlay
-            _imageOverlay = new RenderTargetBitmap(image.PixelWidth, image.PixelHeight, 96, 96, PixelFormats.Pbgra32);
-            if (source.DecoratorFilePath != null)
-            {
-                var decorator = CachedBitmapLoader.LoadFromFile(source.DecoratorFilePath);
-                decorator = new CroppedBitmap(decorator, source.Crop);
-                var vis = new DrawingVisual();
-                using (var ctx = vis.RenderOpen())
-                    ctx.DrawImage(decorator, new Rect(0, 0, decorator.PixelWidth, decorator.PixelHeight));
-                _imageOverlay.Render(vis);
-                source.DecoratorFilePath = null;
-            }
-
-            // add overlay to canvas as child element
-            _rendered = new Image() { Source = _imageOverlay, Stretch = Stretch.Fill, Width = source.UnrotatedBounds.Width, Height = source.UnrotatedBounds.Height };
-            _rendered.RenderTransform = new RotateTransform(source.Angle, (source.Right - source.Left) / 2, (source.Bottom - source.Top) / 2);
-            Canvas.SetLeft(_rendered, source.Left);
-            Canvas.SetTop(_rendered, source.Top);
-            canvas.Children.Add(_rendered);
-
-            _opened = true;
-            Handle(brush, p);
-            return true;
-        }
-
-        private int CalculateHalfPixelSize(ref int brushRadius)
-        {
-            const int minimumPixelSize = 4;
-            var initialBrush = brushRadius;
-
-            var brute = Enumerable.Range(minimumPixelSize, 3).Select(i =>
-            {
-                int closestBrush = 0;
-                while (closestBrush < initialBrush)
-                    closestBrush += i;
-                return new { Pixel = i, Brush = closestBrush };
-            }).OrderBy(x => Math.Abs(x.Brush - initialBrush)).ToArray();
-
-            brushRadius = brute[0].Brush;
-            return brute[0].Pixel;
-        }
-
-        public override void Handle(DrawingBrush brush, Point p)
-        {
-            if (!_opened)
-                throw new InvalidOperationException("Need to open filter before calling Handle");
-
-            // translate mouse point because relative to DPI and to the GraphicImage location / rotation
-            p = _source.UnapplyRotation(p);
-            p.Offset(-_source.Left, -_source.Top);
-            var scaleRatioX = (_imageSource.PixelWidth / _source.UnrotatedBounds.Width);
-            var scaleRatioY = (_imageSource.PixelHeight / _source.UnrotatedBounds.Height);
-            p = new Point(p.X * scaleRatioX, p.Y * scaleRatioY);
-
-            // brushSize must be a multiple of pixelSize and also a multiple of 2 for nice results.
-            int halfBrushSize = brush.Radius;
-            int halfPixelSize = CalculateHalfPixelSize(ref halfBrushSize);
-            int brushSize = halfBrushSize * 2;
-            int pixelSize = halfPixelSize * 2;
-
-            DrawingVisual vis = new DrawingVisual();
-            DrawingContext con = vis.RenderOpen();
-            con.PushOpacityMask(brush.Brush);
-
-            // snap the current rectangle to the grid defined by pixelSize
-            var rect = new Rect(p.X - halfBrushSize, p.Y - halfBrushSize, brushSize, brushSize);
-            double offsetX = -rect.X % pixelSize;
-            if (Math.Abs(offsetX) > halfPixelSize)
-                offsetX += pixelSize;
-            double offsetY = -rect.Y % pixelSize;
-            if (Math.Abs(offsetY) > halfPixelSize)
-                offsetY += pixelSize;
-            rect.Offset(offsetX, offsetY);
-
-            // split rect into smaller "pixels"
-            var n = brushSize / pixelSize;
-            Rect[] smallerRects = new Rect[n * n];
-            for (int y = 0; y < n; y++)
-            {
-                for (int x = 0; x < n; x++)
-                {
-                    smallerRects[x + (y * n)] = new Rect(x * pixelSize + rect.X, y * pixelSize + rect.Y, pixelSize, pixelSize);
-                }
-            }
-
-            var imageRect = new Rect(0, 0, _imageSource.Width, _imageSource.Height);
-            long red = 0, green = 0, blue = 0;
-            int numPixels = pixelSize * pixelSize;
-            foreach (var wpfRect in smallerRects)
-            {
-                // var translatedRect = new Rect(wpfRect.X / dpiRatio, wpfRect.Y / dpiRatio, wpfRect.Width / dpiRatio, wpfRect.Height / dpiRatio);
-                var translatedRect = new Rect(wpfRect.X, wpfRect.Y, wpfRect.Width, wpfRect.Height);
-                if (!imageRect.Contains(translatedRect))
-                    continue;
-
-                // find the average color inside of this rectangle, and draw that color
-                var r = new Int32Rect((int)wpfRect.X, (int)wpfRect.Y, (int)wpfRect.Width, (int)wpfRect.Height);
-                for (int y = 0; y < r.Height; y++)
-                {
-                    for (int x = 0; x < r.Width; x++)
-                    {
-                        var curX = (r.X * 4) + (x * 4);
-                        var curY = (r.Y * _imageStride) + (y * _imageStride);
-                        int pixelStart = curX + curY;
-                        if (pixelStart + 2 >= _imageBytes.Length)
-                        {
-                            blue += 255;
-                            green += 255;
-                            red += 255;
-                        }
-                        else
-                        {
-                            blue += _imageBytes[pixelStart];
-                            green += _imageBytes[pixelStart + 1];
-                            red += _imageBytes[pixelStart + 2];
-                        }
-                    }
-                }
-
-                var avgColor = Color.FromRgb((byte)(red / numPixels), (byte)(green / numPixels), (byte)(blue / numPixels));
-                red = green = blue = 0;
-                con.DrawRectangle(new SolidColorBrush(avgColor), null, wpfRect);
-            }
-
-            con.Close();
-            _imageOverlay.Render(vis);
-        }
-
-        public override void Dispose()
-        {
-            if (!_opened) return;
-
-            var vis = new DrawingVisual();
-            using (var ctx = vis.RenderOpen())
-            {
-                ctx.DrawImage(_imageOverlay, new Rect(_source.Crop.X, _source.Crop.Y, _source.Crop.Width, _source.Crop.Height));
-            }
-
-            var final = new RenderTargetBitmap(_originalSize.Width, _originalSize.Height, 96, 96, PixelFormats.Pbgra32);
-            final.Render(vis);
-
-            var filePath = Path.Combine(Path.GetDirectoryName(_source.BitmapFilePath), Guid.NewGuid() + ".png");
-            var enc = new PngBitmapEncoder();
-            enc.Frames.Add(BitmapFrame.Create(final));
-
-            using (var fs = File.Create(filePath))
-            {
-                enc.Save(fs);
-            }
-
-            _source.DecoratorFilePath = filePath;
-            _canvas.Children.Remove(_rendered);
-            _opened = false;
-        }
-    }
-}
+﻿// using System;
+// using System.IO;
+// using System.Linq;
+// using System.Windows;
+// using System.Windows.Controls;
+// using System.Windows.Media;
+// using System.Windows.Media.Imaging;
+// using Clowd.Drawing.Graphics;
+// using Clowd.PlatformUtil;
+//
+// namespace Clowd.Drawing.Filters
+// {
+//     internal class FilterPixelate : FilterBase
+//     {
+//         private DrawingCanvas _canvas;
+//         private GraphicImage _source;
+//         private BitmapSource _imageSource;
+//         private RenderTargetBitmap _imageOverlay;
+//         private byte[] _imageBytes;
+//         private int _imageStride;
+//         private Image _rendered;
+//         private ScreenSize _originalSize;
+//         private bool _opened;
+//
+//         public override bool Start(DrawingBrush brush, Point p, DrawingCanvas canvas)
+//         {
+//             var clicked = canvas.ToolPointer.MakeHitTest(canvas, p, out var handleNum);
+//             if (clicked is not GraphicImage source)
+//                 return false;
+//
+//             _canvas = canvas;
+//             _source = source;
+//             var image = CachedBitmapLoader.LoadFromFile(source.BitmapFilePath);
+//             _originalSize = new ScreenSize(image.PixelWidth, image.PixelHeight);
+//             image = new CroppedBitmap(image, source.Crop);
+//
+//             // make sure image is in the correct format.
+//             if (image.Format != PixelFormats.Bgra32)
+//             {
+//                 FormatConvertedBitmap formatted = new FormatConvertedBitmap();
+//                 formatted.BeginInit();
+//                 formatted.Source = image;
+//                 formatted.DestinationFormat = PixelFormats.Bgra32;
+//                 formatted.EndInit();
+//                 image = formatted;
+//             }
+//
+//             int nStride = (image.PixelWidth * image.Format.BitsPerPixel + 7) / 8;
+//             byte[] pixelByteArray = new byte[image.PixelHeight * nStride];
+//             image.CopyPixels(pixelByteArray, nStride, 0);
+//             _imageBytes = pixelByteArray;
+//             _imageStride = nStride;
+//             _imageSource = image;
+//
+//             // load any previous pixelations into the current overlay
+//             _imageOverlay = new RenderTargetBitmap(image.PixelWidth, image.PixelHeight, 96, 96, PixelFormats.Pbgra32);
+//             if (source.DecoratorFilePath != null)
+//             {
+//                 var decorator = CachedBitmapLoader.LoadFromFile(source.DecoratorFilePath);
+//                 decorator = new CroppedBitmap(decorator, source.Crop);
+//                 var vis = new DrawingVisual();
+//                 using (var ctx = vis.RenderOpen())
+//                     ctx.DrawImage(decorator, new Rect(0, 0, decorator.PixelWidth, decorator.PixelHeight));
+//                 _imageOverlay.Render(vis);
+//                 source.DecoratorFilePath = null;
+//             }
+//
+//             // add overlay to canvas as child element
+//             _rendered = new Image() { Source = _imageOverlay, Stretch = Stretch.Fill, Width = source.UnrotatedBounds.Width, Height = source.UnrotatedBounds.Height };
+//             _rendered.RenderTransform = new RotateTransform(source.Angle, (source.Right - source.Left) / 2, (source.Bottom - source.Top) / 2);
+//             Canvas.SetLeft(_rendered, source.Left);
+//             Canvas.SetTop(_rendered, source.Top);
+//             canvas.Children.Add(_rendered);
+//
+//             _opened = true;
+//             Handle(brush, p);
+//             return true;
+//         }
+//
+//         private int CalculateHalfPixelSize(ref int brushRadius)
+//         {
+//             const int minimumPixelSize = 4;
+//             var initialBrush = brushRadius;
+//
+//             var brute = Enumerable.Range(minimumPixelSize, 3).Select(i =>
+//             {
+//                 int closestBrush = 0;
+//                 while (closestBrush < initialBrush)
+//                     closestBrush += i;
+//                 return new { Pixel = i, Brush = closestBrush };
+//             }).OrderBy(x => Math.Abs(x.Brush - initialBrush)).ToArray();
+//
+//             brushRadius = brute[0].Brush;
+//             return brute[0].Pixel;
+//         }
+//
+//         public override void Handle(DrawingBrush brush, Point p)
+//         {
+//             if (!_opened)
+//                 throw new InvalidOperationException("Need to open filter before calling Handle");
+//
+//             // translate mouse point because relative to DPI and to the GraphicImage location / rotation
+//             p = _source.UnapplyRotation(p);
+//             p.Offset(-_source.Left, -_source.Top);
+//             var scaleRatioX = (_imageSource.PixelWidth / _source.UnrotatedBounds.Width);
+//             var scaleRatioY = (_imageSource.PixelHeight / _source.UnrotatedBounds.Height);
+//             p = new Point(p.X * scaleRatioX, p.Y * scaleRatioY);
+//
+//             // brushSize must be a multiple of pixelSize and also a multiple of 2 for nice results.
+//             int halfBrushSize = brush.Radius;
+//             int halfPixelSize = CalculateHalfPixelSize(ref halfBrushSize);
+//             int brushSize = halfBrushSize * 2;
+//             int pixelSize = halfPixelSize * 2;
+//
+//             DrawingVisual vis = new DrawingVisual();
+//             DrawingContext con = vis.RenderOpen();
+//             con.PushOpacityMask(brush.Brush);
+//
+//             // snap the current rectangle to the grid defined by pixelSize
+//             var rect = new Rect(p.X - halfBrushSize, p.Y - halfBrushSize, brushSize, brushSize);
+//             double offsetX = -rect.X % pixelSize;
+//             if (Math.Abs(offsetX) > halfPixelSize)
+//                 offsetX += pixelSize;
+//             double offsetY = -rect.Y % pixelSize;
+//             if (Math.Abs(offsetY) > halfPixelSize)
+//                 offsetY += pixelSize;
+//             rect.Offset(offsetX, offsetY);
+//
+//             // split rect into smaller "pixels"
+//             var n = brushSize / pixelSize;
+//             Rect[] smallerRects = new Rect[n * n];
+//             for (int y = 0; y < n; y++)
+//             {
+//                 for (int x = 0; x < n; x++)
+//                 {
+//                     smallerRects[x + (y * n)] = new Rect(x * pixelSize + rect.X, y * pixelSize + rect.Y, pixelSize, pixelSize);
+//                 }
+//             }
+//
+//             var imageRect = new Rect(0, 0, _imageSource.Width, _imageSource.Height);
+//             long red = 0, green = 0, blue = 0;
+//             int numPixels = pixelSize * pixelSize;
+//             foreach (var wpfRect in smallerRects)
+//             {
+//                 // var translatedRect = new Rect(wpfRect.X / dpiRatio, wpfRect.Y / dpiRatio, wpfRect.Width / dpiRatio, wpfRect.Height / dpiRatio);
+//                 var translatedRect = new Rect(wpfRect.X, wpfRect.Y, wpfRect.Width, wpfRect.Height);
+//                 if (!imageRect.Contains(translatedRect))
+//                     continue;
+//
+//                 // find the average color inside of this rectangle, and draw that color
+//                 var r = new Int32Rect((int)wpfRect.X, (int)wpfRect.Y, (int)wpfRect.Width, (int)wpfRect.Height);
+//                 for (int y = 0; y < r.Height; y++)
+//                 {
+//                     for (int x = 0; x < r.Width; x++)
+//                     {
+//                         var curX = (r.X * 4) + (x * 4);
+//                         var curY = (r.Y * _imageStride) + (y * _imageStride);
+//                         int pixelStart = curX + curY;
+//                         if (pixelStart + 2 >= _imageBytes.Length)
+//                         {
+//                             blue += 255;
+//                             green += 255;
+//                             red += 255;
+//                         }
+//                         else
+//                         {
+//                             blue += _imageBytes[pixelStart];
+//                             green += _imageBytes[pixelStart + 1];
+//                             red += _imageBytes[pixelStart + 2];
+//                         }
+//                     }
+//                 }
+//
+//                 var avgColor = Color.FromRgb((byte)(red / numPixels), (byte)(green / numPixels), (byte)(blue / numPixels));
+//                 red = green = blue = 0;
+//                 con.DrawRectangle(new SolidColorBrush(avgColor), null, wpfRect);
+//             }
+//
+//             con.Close();
+//             _imageOverlay.Render(vis);
+//         }
+//
+//         public override void Dispose()
+//         {
+//             if (!_opened) return;
+//
+//             var vis = new DrawingVisual();
+//             using (var ctx = vis.RenderOpen())
+//             {
+//                 ctx.DrawImage(_imageOverlay, new Rect(_source.Crop.X, _source.Crop.Y, _source.Crop.Width, _source.Crop.Height));
+//             }
+//
+//             var final = new RenderTargetBitmap(_originalSize.Width, _originalSize.Height, 96, 96, PixelFormats.Pbgra32);
+//             final.Render(vis);
+//
+//             var filePath = Path.Combine(Path.GetDirectoryName(_source.BitmapFilePath), Guid.NewGuid() + ".png");
+//             var enc = new PngBitmapEncoder();
+//             enc.Frames.Add(BitmapFrame.Create(final));
+//
+//             using (var fs = File.Create(filePath))
+//             {
+//                 enc.Save(fs);
+//             }
+//
+//             _source.DecoratorFilePath = filePath;
+//             _canvas.Children.Remove(_rendered);
+//             _opened = false;
+//         }
+//     }
+// }
---
+++ src/Clowd.Drawing/Tools/ToolPixelate.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Clowd.Drawing.Graphics;
+
+namespace Clowd.Drawing.Tools
+{
+    internal class ToolPixelate : ToolBase
+    {
+        internal override ToolActionType ActionType => ToolActionType.Drawing;
+
+        private GraphicSelectionRectangle _selection = new(new Rect(0, 0, 1, 1));
+        
+        public ToolPixelate() : base(Cursors.Cross)
+        {
+        }
+
+        public override void OnMouseDown(DrawingCanvas canvas, MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(canvas, e);
+            Point p = e.GetPosition(canvas);
+            _selection.MoveHandleTo(p, 1);
+            _selection.MoveHandleTo(p, 5);
+            canvas.GraphicsList.Add(_selection);
+        }
+
+        public override void OnMouseMove(DrawingCanvas canvas, MouseEventArgs e)
+        {
+            if (canvas.IsMouseCaptured)
+            {
+                base.OnMouseMove(canvas, e);
+                Point p = e.GetPosition(canvas);
+                _selection.MoveHandleTo(p, 5);
+            }
+        }
+
+        public override void OnMouseUp(DrawingCanvas canvas, MouseButtonEventArgs e)
+        {
+            if (canvas.IsMouseCaptured)
+            {
+                base.OnMouseUp(canvas, e);
+                Point p = e.GetPosition(canvas);
+                _selection.MoveHandleTo(p, 5);
+                canvas.GraphicsList.Remove(_selection);
+                foreach (var g in canvas.GraphicsList.OfType<GraphicImage>())
+                {
+                    g.AddObscuredArea(_selection.UnrotatedBounds);             
+                }
+                canvas.AddCommandToHistory();
+            }
+        }
+    }
+}
