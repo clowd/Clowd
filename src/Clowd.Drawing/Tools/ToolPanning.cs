ab8ec1695aa30999e680fa69a5462ef1ab3f48a9
Caelan <git@caesay.com>
2022-09-13 23:01:23 +0100

    Use WPF bindings to update edited properties.

---
+++ src/Clowd.Drawing/Tools/ToolPanning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Clowd.Drawing.Tools
+{
+    internal class ToolPanning : ToolBase
+    {
+        public ToolPanning() : base(Cursors.SizeAll, SnapMode.None)
+        {
+        }
+    }
+}
