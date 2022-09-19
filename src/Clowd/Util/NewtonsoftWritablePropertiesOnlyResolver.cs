ef903bd91d5ff2645c5fb4bce66044c84c97bed6
Roman Starkov <sourcecode@roman.st>
2022-09-19 15:33:16 +0100

    Ignore read-only properties when serialising sessions

---
+++ src/Clowd/Util/NewtonsoftWritablePropertiesOnlyResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Clowd.Util;
+
+class NewtonsoftWritablePropertiesOnlyResolver : DefaultContractResolver
+{
+    protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+    {
+        IList<JsonProperty> props = base.CreateProperties(type, memberSerialization);
+        return props.Where(p => p.Writable).ToList();
+    }
+}
