0e0aa2dda74b13996dfa279e4cf8456e79cf36af
Caelan <git@caesay.com>
2024-12-05 12:20:00 +0000

    Remove v3 sources ahead of the v4 rewrite
    v4 is a ground-up rewrite in Rust and shares no code with the v3 C#/.NET
    tree, so there is no incremental path between them. This commit clears the
    v3 sources, letting the v4 history that follows begin from an empty tree.
    
    v3 remains fully reachable through this commit's ancestry.

--- src/Clowd.Drawing/Curves/README.md
+++
@@ -1,4 +0,0 @@
-﻿### A curve fitting library by burningmime. 
-
-- https://gitlab.com/burningmime/curves
-- https://github.com/burningmime/curves
--- src/Clowd.PlatformUtil/ExternalInit.cs
+++
@@ -1,7 +0,0 @@
-﻿// https://stackoverflow.com/a/64749403/184746
-
-namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit
-    { }
-}
