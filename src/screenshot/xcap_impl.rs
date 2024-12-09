0cb8229396b9ccaab607b2d2e181fba5e8d8bf21
Caelan Sayler <git@caesay.com>
2024-12-09 23:17:18 +0000

    add scaling for macos

--- src/screenshot/xcap_impl.rs
+++ src/screenshot/xcap_impl.rs
@@ -32,10 +32,14 @@ pub fn capture_desktop() -> Result<(ScreenRect, DynamicImage, DynamicImage)> {
 
     // Determine the bounding rectangle
     for monitor in &monitors {
-        min_x = min_x.min(monitor.x());
-        min_y = min_y.min(monitor.y());
-        max_x = max_x.max(monitor.x() + monitor.width() as i32);
-        max_y = max_y.max(monitor.y() + monitor.height() as i32);
+        let scale = monitor.scale_factor();
+        min_x = min_x.min((monitor.x() as f32 * scale) as i32);
+        min_y = min_y.min((monitor.y() as f32 * scale) as i32);
+        max_x = max_x.max(((monitor.x() + monitor.width() as i32) as f32 * scale) as i32);
+        max_y = max_y.max(((monitor.y() + monitor.height() as i32) as f32 * scale) as i32);
+        
+        //max_x = max_x.max(monitor.x() + monitor.width() as i32);
+        //max_y = max_y.max(monitor.y() + monitor.height() as i32);
     }
 
     let desktop_bounds = ScreenRect::from_exact(min_x, min_y, max_x, max_y);
@@ -54,8 +58,9 @@ pub fn capture_desktop() -> Result<(ScreenRect, DynamicImage, DynamicImage)> {
 
         // Extract raw data
         let raw_data = captured.into_raw();
-        let m_width = monitor.width();
-        let m_height = monitor.height();
+        let m_width = (monitor.width() as f32 * monitor.scale_factor()) as u32;
+        let m_height = (monitor.height() as f32 * monitor.scale_factor()) as u32;
+        // let m_height = monitor.height();
 
         // Convert raw monitor image into an image crate buffer
         let monitor_image: ImageBuffer<Rgba<u8>, Vec<u8>> =
@@ -69,6 +74,8 @@ pub fn capture_desktop() -> Result<(ScreenRect, DynamicImage, DynamicImage)> {
             .copy_from(&monitor_image, offset_x, offset_y)
             .expect("Failed to copy monitor image into desktop image");
     }
+    
+    desktop_image.save("screenshot.png").expect("Failed to save screenshot");
 
     // Convert to a DynamicImage
     let dynamic_image = DynamicImage::ImageRgba8(desktop_image);
