6a4fd3f5fb082ba08c48d88314304efdde0dc6a5
Caelan <git@caesay.com>
2024-12-15 20:47:20 +0000

    wip

---
+++ clowd_capture/src/resources.rs
@@ -0,0 +1,42 @@
+#![allow(dead_code)]
+use crate::geometry::*;
+use bevy::prelude::Resource;
+
+#[derive(Resource)]
+pub struct MousePosition(mouse_rs::Mouse);
+
+impl MousePosition {
+    pub fn get(&self) -> ScreenPoint {
+        let pos = self.0.get_position().unwrap();
+        ScreenPoint::new(pos.x as i32, pos.y as i32)
+    }
+    pub fn set(&self, pos: ScreenPoint) {
+        let _ = self.0.move_to(pos.x, pos.y);
+    }
+}
+
+impl Default for MousePosition {
+    fn default() -> Self {
+        MousePosition(mouse_rs::Mouse::new())
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct CaptureState {
+    pub selection: Option<ScreenRect>,
+    pub zoom: f32,
+    pub mouse_state: MouseState,
+    pub mouse_pos: ScreenPointF,
+}
+
+#[derive(Resource, Default)]
+pub enum MouseState {
+    #[default]
+    Up,
+    StartSel(ScreenPointF),
+}
+
+// #[derive(Resource, Default)]
+// pub struct RenderSettings {
+//     pub debug: bool,
+// }
