da62780e18b398b6bd14450da802825b0ebeb8e2
Caelan <git@caesay.com>
2024-12-18 11:47:21 +0000

    replace icons

--- clowd_ui/src-tauri/src/lib.rs
+++ clowd_ui/src-tauri/src/lib.rs
@@ -1,4 +1,4 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,6 +6,9 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // let tray_menu = SystemTrayMenu::new(); // insert the menu items here
+    // let system_tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
